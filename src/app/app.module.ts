08826830fa1104abe68aa6fdd8e38e124e1ccf8b
leomendoza123 <leomendoza123@gmail.com>
2018-11-26 13:29:57 -0600

    Add core and shared module
    This follows the basic angular module structure recommended on Angular documentation see more on the following URL
    https://angular.io/guide/ngmodule-faq#what-kinds-of-modules-should-i-have-and-how-should-i-use-them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { LayoutModule } from './layout/layout.module'
+import { CoreModule } from './core/core.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common'
 
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './pages/home/home.component'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
