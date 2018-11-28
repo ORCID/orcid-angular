1c11ef4f92c10d9aa6218fd94ede237a6690fc32
leomendoza123 <leomendoza123@gmail.com>
2018-11-28 08:24:18 -0600

    Add angular material and fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { LayoutModule } from './layout/layout.module'
 import { CoreModule } from './core/core.module'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, LayoutModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
