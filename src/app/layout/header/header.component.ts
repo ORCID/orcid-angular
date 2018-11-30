d2fe8d752ae8dbb6e3a0ab876b6c3f4787b200ba
Leonardo Mendoza Fernández <leomendoza123@gmail.com>
2018-11-30 17:39:03 -0600

    Add basic menu functionality

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core'
+import { Router, NavigationEnd } from '@angular/router'
+import { filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-header',
@@ -6,24 +8,80 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  public menu = {
-    'FOR RESEARCHERS': [],
-    'FOR ORGANIZATIONS': [],
-    ABOUT: [],
-    HELP: [],
-    'SIGN IN': [],
+  private currentRoute
+  private menu = {
+    researchers: {
+      hover: true,
+      route: '/',
+      buttons: {
+        signIn: {},
+        signUp: {},
+        learMore: {},
+      },
+    },
+    organizations: {
+      hover: false,
+      active: false,
+      buttons: {
+        funders: {},
+        publishers: {},
+        organizations: {},
+        outreach: {},
+        api: {},
+      },
+    },
+    about: {
+      hover: false,
+      active: false,
+      buttons: {
+        what: {},
+        are: {},
+        serve: {},
+        membership: {},
+        events: {},
+      },
+    },
+    help: {
+      hover: false,
+      active: false,
+      buttons: {
+        help: {},
+        faq: {},
+        base: {},
+        feedback: {},
+      },
+    },
+    signIn: {
+      hover: false,
+      active: false,
+    },
   }
-  constructor() {
-    console.log(Object.keys(this.menu))
+  constructor(router: Router) {
+    router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd))
+      .subscribe(val => {
+        this.currentRoute = router.url
+      })
   }
 
   ngOnInit() {}
 
-  mouseEnter(div: string) {
-    console.log('mouse enter : ' + div)
+  mouseEnter(ul: string) {
+    Object.keys(this.menu).forEach(button => {
+      if (button === ul) {
+        this.menu[button].hover = true
+      } else {
+        this.menu[button].hover = false
+      }
+    })
+    this.menu[ul].hover = true
   }
 
-  mouseLeave(div: string) {
-    console.log('mouse leave :' + div)
+  mouseLeave() {
+    Object.keys(this.menu).forEach(button => {
+      this.menu[button].hover = this.menu[button].route === this.currentRoute
+    })
   }
+
+  click(ul: string) {}
 }
