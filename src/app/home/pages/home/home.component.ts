e75f9eed7a3bad1dff563f43a6d31874492423f7
Leonardo Mendoza Fernández <leomendoza123@gmail.com>
2018-12-04 21:58:51 -0600

    Add home basic structure
    - add @material/layout-grid
    - add home.component.scss-theme

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { HostBinding } from '@angular/core'
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  @HostBinding('class') class = 'mdc-layout-grid__inner'
+
   constructor() {}
 
   ngOnInit() {}
