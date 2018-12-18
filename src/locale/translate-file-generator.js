9971855740eda5a7128f5e9b25656f8a1506a501
Leonardo Mendoza Fernández <leomendoza123@gmail.com>
2018-12-18 10:56:42 -0600

    Refactor the translation file generator
    - ignores generated files
    - adds package json scripts

--- src/locale/translate-file-generator.js
+++ src/locale/translate-file-generator.js
@@ -5,6 +5,16 @@ const util = require('util')
 const propertiesToJSON = require('properties-to-json')
 const axios = require('axios')
 
+baseGithubTranslationFilesURL = [
+  'https://raw.githubusercontent.com/ORCID/ORCID-Source/master/orcid-core/src/main/resources/i18n/about_',
+  'https://raw.githubusercontent.com/ORCID/ORCID-Source/master/orcid-core/src/main/resources/i18n/messages_',
+  'https://raw.githubusercontent.com/ORCID/ORCID-Source/master/orcid-core/src/main/resources/i18n/admin_',
+  'https://raw.githubusercontent.com/ORCID/ORCID-Source/master/orcid-core/src/main/resources/i18n/email_',
+  'https://raw.githubusercontent.com/ORCID/ORCID-Source/master/orcid-core/src/main/resources/i18n/identifiers_',
+  'https://raw.githubusercontent.com/ORCID/ORCID-Source/master/orcid-core/src/main/resources/i18n/javascript_',
+  'https://raw.githubusercontent.com/ORCID/ORCID-Source/master/orcid-core/src/main/resources/i18n/test_messages_',
+]
+
 languages = [
   {
     code: 'ar',
@@ -58,77 +68,124 @@ languages = [
     code: 'zh_TW',
   },
 ]
+unfoundTranslations = {}
 
-languages.forEach(language => {
-  generateLanguageFile(language.code, language.code)
+readMessageFile('./src/locale/messages.xlf', 'utf-8').then(file => {
+  Promise.all(
+    languages.map(language =>
+      generateLanguageFile(language.code, language.code, deepCopy(file))
+    )
+  ).then(() => {
+    saveJson(unfoundTranslations, 'unfound-translations')
+  })
 })
 
-function generateLanguageFile(code, saveCode) {
-  let language
-  return new Promise((resolve, reject) => {
-    Promise.all([
-      axios
-        .get(
-          'https://raw.githubusercontent.com/ORCID/ORCID-Source/master/orcid-core/src/main/resources/i18n/about_' +
-            code +
-            '.properties'
-        )
-        .catch(error => {
-          return null
-        }),
-      axios
-        .get(
-          'https://raw.githubusercontent.com/ORCID/ORCID-Source/master/orcid-core/src/main/resources/i18n/messages_' +
-            code +
-            '.properties'
-        )
-        .catch(error => {
-          return null
-        }),
-      ,
-    ])
-      .then(data => {
-        let propsText = {}
-        data.forEach(data => {
-          if (data && data.data)
-            Object.assign(propsText, propertiesToJSON(data.data))
-        })
-        return propsText
-      })
-      .then(propsText => {
-        fs.readFile('./src/locale/messages.fr.xlf', 'utf-8', (error, data) => {
-          if (error) {
-            console.log(error)
-            //TODO THROW
-          }
-          parseString(data, (error, result) => {
-            if (error) {
-              console.log(error)
+function generateLanguageFile(code, saveCode, file) {
+  return Promise.all(
+    baseGithubTranslationFilesURL.map(url =>
+      getTranslationFileFromGithub(url, code)
+    )
+  )
+    .then(data => getProperties(data))
+    .then(propsText =>
+      setLanguagePropertiesToLanguageFile(file, propsText, saveCode)
+    )
+    .then(result => saveJsonAsXlf(result, saveCode))
+    .then(() =>
+      console.log('successfully create translation file for ' + saveCode)
+    )
+}
 
-              //TODO THROW
-            }
-            result.xliff.file[0].unit.forEach(element => {
-              if (propsText[element['$'].id]) {
-                element.segment[0].target = []
-                element.segment[0].target.push(propsText[element['$'].id])
-              }
-            })
-            // create a new builder object and then convert
-            // our json back to xml.
-            var builder = new xml2jsBuilder()
-            var xml = builder.buildObject(result)
-            fs.writeFile(
-              './src/locale/messages.' + saveCode + '.xlf',
-              xml,
-              function(err, data) {
-                if (err) console.log(err)
-                console.log(
-                  'successfully create translation file for ' + saveCode
-                )
-              }
-            )
-          })
-        })
+function saveJsonAsXlf(json, name) {
+  new Promise((resolve, reject) => {
+    var builder = new xml2jsBuilder()
+    var xml = builder.buildObject(json)
+    fs.writeFile('./src/locale/messages.' + name + '.xlf', xml, function(
+      err,
+      data
+    ) {
+      if (err) {
+        reject(err)
+      }
+      resolve()
+    })
+  })
+}
+
+function saveJson(json, name) {
+  new Promise((resolve, reject) => {
+    fs.writeFile(
+      './src/locale/messages.' + name + '.json',
+      JSON.stringify(unfoundTranslations, null, 2),
+      function(err) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve()
+        }
+      }
+    )
+  })
+}
+
+function getProperties(data) {
+  let propsText = {}
+  data.forEach(data => {
+    if (data && data.data) {
+      Object.assign(propsText, propertiesToJSON(data.data))
+    }
+  })
+  return propsText
+}
+
+function readMessageFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+function setLanguagePropertiesToLanguageFile(data, propsText, saveCode) {
+  return new Promise((resolve, reject) => {
+    parseString(data, (error, result) => {
+      if (error) {
+        reject(error)
+      }
+      //console.log(result)
+      result.xliff.file[0].unit.forEach(element => {
+        if (propsText[element['$'].id]) {
+          element.segment[0].target = []
+          element.segment[0].target.push(propsText[element['$'].id])
+        } else {
+          translationNotFound(element['$'].id, saveCode)
+        }
       })
+      resolve(result)
+    })
+  })
+}
+
+function deepCopy(json) {
+  return JSON.parse(JSON.stringify(json))
+}
+
+function translationNotFound(id, saveCode) {
+  if (!unfoundTranslations[saveCode]) {
+    unfoundTranslations[saveCode] = []
+  }
+  unfoundTranslations[saveCode].push(id)
+}
+
+function getTranslationFileFromGithub(baseUrl, code) {
+  return axios.get(baseUrl + code + '.properties').catch(error => {
+    if (!unfoundTranslations.unexistingFiles) {
+      unfoundTranslations.unexistingFiles = []
+    }
+    unfoundTranslations.unexistingFiles.push(baseUrl + code + '.properties')
   })
 }
