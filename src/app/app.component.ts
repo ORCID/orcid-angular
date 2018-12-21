4cd85d22074324eb6a9cbee37e54b5951f385c9f
Leonardo Mendoza Fernández <leomendoza123@gmail.com>
2018-12-20 22:57:09 -0600

    Add RTL language support
    - move the tablet, ie, edge an other clases related to the platform to app-root
    - add index.rtl.html to swap the index.html on RTL languages at build time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core'
 import { BreakpointObserver } from '@angular/cdk/layout'
 import { Breakpoints } from '@angular/cdk/layout'
 import { Platform } from '@angular/cdk/platform'
+import { Directionality } from '@angular/cdk/bidi'
+import { HostBinding } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -9,26 +11,27 @@ import { Platform } from '@angular/cdk/platform'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-orcid'
-  tablet
-  handset
-  ie
-  edge
+  @HostBinding('class.edge') edge
+  @HostBinding('class.ie') ie
+  @HostBinding('class.tabletOrHandset') tabletOrHandset
+  @HostBinding('class.handset') handset
+  @HostBinding('class.tablet') tablet
+  @HostBinding('class.desktop') desktop
+
   constructor(
     private _breakpointObserver: BreakpointObserver,
     private _platform: Platform
   ) {
     this.ie = _platform.TRIDENT
     this.edge = _platform.EDGE
-    this._breakpointObserver
-      .observe([Breakpoints.Handset, Breakpoints.Tablet])
-      .subscribe(state => {
-        if (state.matches) {
-          this.handset = true
-        } else {
-          this.handset = false
-        }
-      })
+
+    this._breakpointObserver.observe([Breakpoints.Handset]).subscribe(state => {
+      if (state.matches) {
+        this.handset = true
+      } else {
+        this.handset = false
+      }
+    })
     this._breakpointObserver.observe([Breakpoints.Tablet]).subscribe(state => {
       if (state.matches) {
         this.tablet = true
@@ -36,5 +39,16 @@ export class AppComponent {
         this.tablet = false
       }
     })
+    this._breakpointObserver
+      .observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .subscribe(state => {
+        if (state.matches) {
+          this.tabletOrHandset = true
+          this.desktop = false
+        } else {
+          this.tabletOrHandset = false
+          this.desktop = true
+        }
+      })
   }
 }
