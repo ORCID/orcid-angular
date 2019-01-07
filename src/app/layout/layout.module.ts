27756e6dadfd3dd4cd0771b3a9bacd674c954469
Leonardo Mendoza Fernández <leomendoza123@gmail.com>
2019-01-07 10:12:49 -0600

    Add logo navigation to home page
    - add loading to profile-general-data component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common'
 import { HeaderComponent } from './header/header.component'
 import { FooterComponent } from './footer/footer.component'
 import { MatMenuModule, MatButtonModule } from '@angular/material'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
-  imports: [CommonModule, MatMenuModule, MatButtonModule],
+  imports: [CommonModule, MatMenuModule, MatButtonModule, RouterModule],
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { MatDividerModule, MatDivider } from '@angular/material'
+import {
+  MatDividerModule,
+  MatDivider,
+  MatProgressSpinnerModule,
+  MatProgressSpinner,
+} from '@angular/material'
 import { MatTooltipModule } from '@angular/material/tooltip'
 
 @NgModule({
-  imports: [CommonModule, MatDividerModule],
+  imports: [CommonModule, MatDividerModule, MatProgressSpinnerModule],
   declarations: [],
-  exports: [MatDivider, MatTooltipModule],
+  exports: [MatDivider, MatTooltipModule, MatProgressSpinner],
   providers: [], // Should not provide anything
 })
 export class SharedModule {}
