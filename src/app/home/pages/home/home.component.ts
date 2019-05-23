c48742aa77687346744207ec6160f4b18765a6f1
leomendoza123 <leomendoza123@gmail.com>
2019-05-22 19:27:48 -0600

    UI fixes
    - fix hero header space error
    - support multiple sizes text
    - stop usign window directly

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Inject } from '@angular/core'
 import { enterAnimation } from 'src/app/animations'
-import { PlatformInfoService } from 'src/app/core'
+import { WINDOW, PlatformInfoService } from 'src/app/core'
+import { BrowserWindowRef } from 'src/app/core/window/window.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   animations: [enterAnimation],
+  preserveWhitespaces: true,
 })
 export class HomeComponent implements OnInit {
   platform
 
-  constructor(_platformInfo: PlatformInfoService) {
+  constructor(
+    _platformInfo: PlatformInfoService,
+    @Inject(WINDOW) private window: Window
+  ) {
     _platformInfo.get().subscribe(platformInfo => {
       this.platform = platformInfo
     })
+  }
+
+  navigateTo(val) {
+    this.window.location.href = val
   }
 
   ngOnInit() {}
