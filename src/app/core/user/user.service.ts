c71cf0b100d97f12dc65144b0102f3e216e8380b
leomendoza123 <leomendoza123@gmail.com>
2019-06-19 19:49:59 -0600

    Display user name

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { UserInfo } from 'src/app/types'
-import { timer, Observable, of, Subject } from 'rxjs'
+import { UserInfo, NameForm } from 'src/app/types'
+import { timer, Observable, of, Subject, forkJoin } from 'rxjs'
 import {
   switchMap,
   tap,
@@ -23,7 +23,11 @@ export class UserService {
   constructor(private _http: HttpClient) {}
   private currentlyLoggedIn = true
   private loggingStateComesFromTheServer = false
-  private $infoOnEachStatusUpdateObservable: Observable<UserInfo>
+  private $infoOnEachStatusUpdateObservable: Observable<{
+    userInfo: UserInfo
+    nameForm: NameForm
+    displayName: string
+  }>
 
   private getUserInfo(): Observable<UserInfo> {
     return this._http.get<UserInfo>(environment.API_WEB + 'userInfo.json', {
@@ -40,7 +44,20 @@ export class UserService {
       .pipe(map(response => response.loggedIn || null))
   }
 
-  getUserInfoOnEachStatusUpdate(): Observable<UserInfo> {
+  private getNameForm(): Observable<NameForm> {
+    return this._http.get<NameForm>(
+      environment.API_WEB + 'account/nameForm.json',
+      {
+        withCredentials: true,
+      }
+    )
+  }
+
+  getUserInfoOnEachStatusUpdate(): Observable<{
+    userInfo: UserInfo
+    nameForm: NameForm
+    displayName: string
+  }> {
     // If an observable already exists, the same is shared between subscriptions
     // If not creates an observable
     if (this.$infoOnEachStatusUpdateObservable) {
@@ -59,7 +76,6 @@ export class UserService {
           .pipe(
             filter(loggedIn => {
               if (loggedIn) {
-                this.currentlyLoggedIn = false
                 this.loggingStateComesFromTheServer = true
                 if (!(loggedIn === this.currentlyLoggedIn)) {
                   this.currentlyLoggedIn = loggedIn
@@ -70,9 +86,9 @@ export class UserService {
             })
           )
           // At the very beginning assumes the user is logged in,
-          // this is to avoid waiting for userStatus.json before calling userInfo.json on the first load
+          // this is to avoid waiting for userStatus.json before calling userInfo.json and nameForm.json on the first load
           .pipe(startWith(true))
-          // If the user is logged in get the UserStatus.json
+          // If the user is logged in get the UserStatus.json and nameForm.json
           // If not return a null value
           .pipe(
             switchMap(loggedIn => {
@@ -80,18 +96,48 @@ export class UserService {
                 return of(null)
               }
               if (loggedIn) {
-                return this.getUserInfo().pipe(this.handleErrors)
+                // return an object with the most recent response of both endpoints
+                return forkJoin({
+                  userInfo: this.getUserInfo().pipe(this.handleErrors),
+                  nameForm: this.getNameForm().pipe(this.handleErrors),
+                })
+              }
+            }),
+            map((data: { userInfo: UserInfo; nameForm: NameForm }) => {
+              // computes the name that should be displayed on the UI
+              return {
+                ...data,
+                ...{ displayName: this.getDisplayName(data.nameForm) },
               }
             })
           )
           .pipe(shareReplay(1)))
     }
   }
-  private handleErrors(gerUserInfo: Observable<UserInfo>) {
+  private getDisplayName(nameForm: NameForm): string {
+    if (nameForm != null) {
+      if (
+        nameForm.creditName &&
+        nameForm.creditName.value &&
+        nameForm.creditName.value.length
+      ) {
+        return nameForm.creditName.value
+      } else {
+        return (
+          ((nameForm.givenNames && nameForm.givenNames.value) || ' ') +
+          ' ' +
+          ((nameForm.familyName && nameForm.familyName.value) || ' ')
+        )
+      }
+    } else {
+      return null
+    }
+  }
+  private handleErrors(gerUserInfo: Observable<UserInfo | NameForm>) {
     return (
       gerUserInfo
         .pipe(
-          // If UserInfo.json give an error it retries only if the user is currently logged in
+          // If UserInfo.json or nameForm.json give an error it retries only if the user is currently logged in
           //
           // This is necessary because in some cases when the userStatus.json responded with { logging = true }
           // and the userInfo.json is called immediately after it responds with an error
--- src/app/layout/user-menu/user-menu.component.ts
+++ src/app/layout/user-menu/user-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core'
 import { UserService, WINDOW, PlatformInfoService } from 'src/app/core'
 import { environment } from 'src/environments/environment'
-import { PlatformInfo, UserInfo } from 'src/app/types'
+import { PlatformInfo, UserInfo, NameForm } from 'src/app/types'
 
 @Component({
   selector: 'app-user-menu',
@@ -11,6 +11,7 @@ import { PlatformInfo, UserInfo } from 'src/app/types'
 export class UserMenuComponent implements OnInit {
   state = false
   userInfo: UserInfo
+  displayName: string
   platform: PlatformInfo
   constructor(
     _userInfo: UserService,
@@ -18,7 +19,8 @@ export class UserMenuComponent implements OnInit {
     _platform: PlatformInfoService
   ) {
     _userInfo.getUserInfoOnEachStatusUpdate().subscribe(data => {
-      this.userInfo = data
+      this.userInfo = data.userInfo
+      this.displayName = data.displayName
     })
     _platform.get().subscribe(data => {
       this.platform = data
