12d6a73b1eff94ea444bb62290331fe3ee3ed20c
leomendoza123 <leomendoza123@gmail.com>
2019-09-06 12:29:41 -0600

    Add password recovery service

--- src/app/core/password-recovery/password-recovery.service.ts
+++ src/app/core/password-recovery/password-recovery.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { ErrorHandlerService } from '../error-handler/error-handler.service'
+import { PasswordRecovery } from 'src/app/types'
+import { environment } from 'src/environments/environment'
+import { retry, catchError } from 'rxjs/operators'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PasswordRecoveryService {
+  constructor(
+    private _http: HttpClient,
+    private _errorHandler: ErrorHandlerService
+  ) {}
 
-  constructor() { }
+  resetPassword(data) {
+    return this._http
+      .post<PasswordRecovery>(
+        environment.API_WEB + `reset-password.json`,
+        data,
+        { withCredentials: true }
+      )
+      .pipe(
+        retry(0),
+        catchError(this._errorHandler.handleError)
+      )
+  }
 }
