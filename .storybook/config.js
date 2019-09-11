3979fe658e0faa79d2fce60a224c9025b990e533
Leonardo Mendoza Fernández <leomendoza123@gmail.com>
2019-09-11 15:54:20 -0600

    Add missing libraries

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,18 @@
-import { configure } from '@storybook/angular'
+import { configure, addParameters, addDecorator } from '@storybook/angular'
+import { withA11y } from '@storybook/addon-a11y'
 
+addDecorator(withA11y)
+//
+addParameters({
+  options: {
+    hierarchyRootSeparator: /\|/,
+  },
+  docs: {
+    iframeHeight: '60px',
+  },
+})
 // automatically import all files ending in *.stories.ts
-const req = require.context('../src/stories', true, /\.stories\.ts$/)
-function loadStories() {
-  req.keys().forEach(filename => req(filename))
-}
-
-configure(loadStories, module)
+configure(
+  require.context('../src/stories', true, /\.stories\.(ts|mdx)$/),
+  module
+)
