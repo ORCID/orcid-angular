8889dd7020bd4ef6fce8b97c7c55595400ee6517
lizkrznarich <e.krznarich@orcid.org>
2019-10-14 19:43:32 -0500

    environment definition for local tomcat

---
+++ src/environments/environment.local.tomcat.ts
@@ -0,0 +1,36 @@
+export const environment = {
+  production: false,
+  API_NEWS: 'https://localhost/blog/feed',
+  API_WEB: 'https://localhost/',
+  BASE_URL: 'https://localhost/',
+  BLOG_NEWS: 'https://localhost/about/news',
+  LANGUAGE_MENU_OPTIONS: {
+    ar: 'العربية',
+    cs: 'Čeština',
+    en: 'English',
+    es: 'Español',
+    fr: 'Français',
+    it: 'Italiano',
+    ja: '日本語',
+    ko: '한국어',
+    pt: 'Português',
+    ru: 'Русский',
+    'zh-CN': '简体中文',
+    'zh-TW': '繁體中文',
+    xx: '** xx',
+    source: '** source',
+    lr: '** lr',
+    rl: '** rl',
+    uk: '** Ukrainian',
+    ca: '** Catalan',
+  },
+}
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
