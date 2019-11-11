f2933a466f9caf0cfd80212662f8271523fba7ec
leomendoza123 <leomendoza123@gmail.com>
2019-11-11 16:24:25 -0600

    Add cookie base language support

--- src/app/layout/language/language.component.ts
+++ src/app/layout/language/language.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject, LOCALE_ID } from '@angular/core'
 import { environment } from 'src/environments/environment'
 import { WINDOW } from 'src/app/core'
+import { CookieService } from 'ngx-cookie-service'
 
 @Component({
   selector: 'app-language',
@@ -11,13 +12,19 @@ export class LanguageComponent implements OnInit {
   languageMenuOptions
   constructor(
     @Inject(LOCALE_ID) public locale: string,
-    @Inject(WINDOW) private window: Window
+    @Inject(WINDOW) private window: Window,
+    private _cs: CookieService
   ) {
     this.languageMenuOptions = environment.LANGUAGE_MENU_OPTIONS
   }
 
   changeLanguage(languageKey: string) {
-    this.window.location.href = '/' + languageKey + '/'
+    if (environment.production) {
+      this._cs.set('locale_v3', languageKey)
+      this.window.location.reload()
+    } else {
+      this.window.location.href = '/' + languageKey + '/'
+    }
   }
   ngOnInit() {}
 }
