930d6173cf1e2b411b0820bf987ebe6c74ad874b
leomendoza123 <leomendoza123@gmail.com>
2019-11-27 20:38:38 -0600

    Generate `is this you` modal with the CLI

---
+++ src/app/sign-in/components/is-this-you/is-this-you.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-is-this-you',
+  templateUrl: './is-this-you.component.html',
+  styleUrls: ['./is-this-you.component.scss'],
+})
+export class IsThisYouComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/sign-in/sign-in.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { IsThisYouComponent } from './components/is-this-you/is-this-you.component'
+
+@NgModule({
+  declarations: [IsThisYouComponent],
+  imports: [CommonModule],
+})
+export class SignInModule {}
