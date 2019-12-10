ee1d97e8429ae9180b35dc7d23bf598f511fe17a
Leonardo Mendoza Fernández <leomendoza123@gmail.com>
2019-12-10 16:04:58 -0600

    Add unique length post build script docs
    - Update language files

--- scripts/unique-length.postbuild.ts
+++ scripts/unique-length.postbuild.ts
@@ -1,3 +1,20 @@
+/*
+The goal of this script is to add different comment lengths on the index.html files of each language.
+The reason for doing this is to have different eTag HTTP response headers for each.
+This works since Nginx generates etags based on the file lengths.
+Different etags are required for each language since browsers use this to know whether it can reuse the
+cached app (304 response) or a new app with a different language should be downloaded (200 response).
+
+Know issues with Cloudflare.
+
+1- If HTML minimization is activated this will remove comments on the "index.html" and different languages
+will have the same eTag. Also, the Nginx security feature of adding a random length HTML comment will be removed.
+Because of that, this minification should be avoided.
+
+2-  As described on the official docs to enable weak etags the features "Email Obfuscation" and
+"Automatic HTTPS Rewrites" must be disabled.
+https://support.cloudflare.com/hc/en-us/articles/218505467-Using-ETag-Headers-with-Cloudflare 
+*/
 export function uniqueLength(indexHtml, options): string {
   const configurationLanguage = options.languageCode
   const languageUniqueLength = getLanguageUniqueLength(
