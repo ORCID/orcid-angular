0eeee23005fdd86101c003a20526c22a7685e844
Leonardo Mendoza Fernández <leomendoza123@gmail.com>
2019-12-16 17:22:29 -0600

    Add report summary

--- scripts/lighthouse.ts
+++ scripts/lighthouse.ts
@@ -1,6 +1,9 @@
 import lighthouse from 'lighthouse/lighthouse-core'
 import { launch } from 'chrome-launcher/chrome-launcher'
 import * as firebase from 'firebase'
+import * as fs from 'fs'
+const log = require('lighthouse-logger')
+
 const puppeteer = require('puppeteer')
 
 let _firebase
@@ -21,26 +24,71 @@ async function initializeFirebase() {
 }
 
 async function addRecord(result) {
-  console.log(result)
+  fs.writeFile(`${'report'}.json`, JSON.stringify(result), err => {
+    if (err) {
+    }
+    console.log(`Lighthouse JSON report saved`)
+  })
+
   const fullDataId = await _firebaseDB
     .collection('audits')
     .add({ JSON: JSON.stringify(result) })
     .then(function(docRef) {
       console.log('Document written with ID: ', docRef.id)
+      return docRef
     })
     .catch(function(error) {
+      // THROW AND ERROR
       console.error('Error adding document: ', error)
     })
 
+  const summary = {
+    fetchTime: result.fetchTime,
+    requestedUrl: result.requestedUrl,
+    metrics: generateTrackableReport(result),
+    fullReportRef: fullDataId,
+  }
+
+  console.log(summary)
+
   const summaryId = await _firebaseDB
     .collection('summary')
-    .add({ metrics: result.audits.metrics, full: 'fullDataId' })
+    .add(summary)
     .then(function(docRef) {
       console.log('Document written with ID: ', docRef.id)
     })
     .catch(function(error) {
       console.error('Error adding document: ', error)
     })
+}
+
+// Pulling out the metrics we are interested in
+function generateTrackableReport(audit) {
+  const reports = [
+    'first-contentful-paint',
+    'first-meaningful-paint',
+    'speed-index',
+    'interactive',
+    'first-cpu-idle',
+    'estimated-input-latency',
+  ]
+
+  const obj = {
+    results: {},
+  }
+
+  reports.forEach(report => {
+    obj.results[report] = getRequiredAuditMetrics(audit.audits[report])
+  })
+  return obj
+}
+
+function getRequiredAuditMetrics(metrics) {
+  return {
+    id: metrics.id,
+    score: metrics.score,
+    value: metrics.numericValue,
+  }
 }
 
 async function launchChromeAndRunLighthouse(url, opts, config = null) {
@@ -54,7 +102,7 @@ async function launchChromeAndRunLighthouse(url, opts, config = null) {
   const browser = await puppeteer.launch({
     args: [`--remote-debugging-port=${PORT}`],
     headless: false,
-    slowMo: 50,
+    // slowMo: 50,
     executablePath:
       '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
   })
@@ -63,7 +111,7 @@ async function launchChromeAndRunLighthouse(url, opts, config = null) {
   await page.goto(`${url}/signin`)
 
   const emailInput = await page.$('input[id="userId"]')
-  await emailInput.type('****@gmail.com')
+  await emailInput.type('***@gmail.com')
   const passwordInput = await page.$('input[type="password"]')
   await passwordInput.type('****')
   await Promise.all([
@@ -71,11 +119,12 @@ async function launchChromeAndRunLighthouse(url, opts, config = null) {
     page.waitForNavigation(),
   ])
 
-  opts.port = browser.port
-  console.log('after launch')
+  opts.port = PORT
+  opts.enableErrorReporting = true
+  opts.logLevel = 'info'
+  log.setLevel(opts.logLevel)
   try {
-    const result = await lighthouse(url, { port: PORT })
-    console.log('okok')
+    const result = await lighthouse(url, opts)
     return result.lhr
   } finally {
     console.log('after kill')
@@ -83,27 +132,18 @@ async function launchChromeAndRunLighthouse(url, opts, config = null) {
   }
 }
 
-const opts = {
-  chromeFlags: ['--show-paint-rects'],
-}
-
 // Usage:
 
 async function test() {
+  const opts = {
+    chromeFlags: ['--show-paint-rects'],
+  }
+
   try {
     await initializeFirebase()
 
     let result = await launchChromeAndRunLighthouse(
-      'https://orcid.org/myorcid',
-      opts
-    )
-    await addRecord(result)
-
-    result = await launchChromeAndRunLighthouse('https://qa.orcid.org', opts)
-    await addRecord(result)
-
-    result = await launchChromeAndRunLighthouse(
-      'https://sandbox.orcid.org',
+      'https://orcid.org/my-orcid',
       opts
     )
     await addRecord(result)
