71f6b1915ad9f495f941408ef4210fb27d940860
leomendoza123 <leomendoza123@gmail.com>
2020-01-10 12:21:56 -0600

    Mix remote version of platforInfo bit component

--- src/app/cdk/is-this-you/is-this-you.module.ts
+++ src/app/cdk/is-this-you/is-this-you.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { IsThisYouComponent } from './is-this-you.component'
 import { MatButtonModule } from '@angular/material'
+import { PlatformInfoModule } from '../platform-info'
 
 @NgModule({
   declarations: [IsThisYouComponent],
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, PlatformInfoModule],
   exports: [IsThisYouComponent],
 })
 export class IsThisYouModule {}
