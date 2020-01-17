e40fc6c4a18bfc83d9b61726951c91c453ba4acc
leomendoza123 <leomendoza123@gmail.com>
2020-01-17 12:35:15 -0600

    Remove is-this-you component from the sing-in module

--- src/app/sign-in/pages/sign-in/sign-in.component.ts
+++ src/app/sign-in/pages/sign-in/sign-in.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core'
 import { MatDialog } from '@angular/material/dialog'
-import { IsThisYouComponent } from 'src/app/cdk/is-this-you/is-this-you.component'
 
 @Component({
   selector: 'app-sign-in',
@@ -8,74 +7,7 @@ import { IsThisYouComponent } from 'src/app/cdk/is-this-you/is-this-you.componen
   styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-  duplicateRecords = [
-    {
-      orcid: '0000-0003-0625-2121',
-      email: null,
-      givenNames: 'Affiliations',
-      familyNames: null,
-      institution: 'Public Education, Public Affiliation',
-    },
-    {
-      orcid: '0000-0003-0625-2121',
-      email: null,
-      givenNames: 'Affiliations',
-      familyNames: null,
-      institution: 'Public Education, Public Affiliation',
-    },
-    {
-      orcid: '0000-0003-0625-2121',
-      email: null,
-      givenNames: 'Affiliations',
-      familyNames: null,
-      institution: 'Public Education, Public Affiliation',
-    },
-    {
-      orcid: '0000-0003-0625-2121',
-      email: null,
-      givenNames: 'Affiliations',
-      familyNames: null,
-      institution: 'Public Education, Public Affiliation',
-    },
-    {
-      orcid: '0000-0003-0625-2121',
-      email: null,
-      givenNames: 'Affiliations',
-      familyNames: null,
-      institution: 'Public Education, Public Affiliation',
-    },
-    {
-      orcid: '0000-0003-0625-2121',
-      email: null,
-      givenNames: 'Affiliations',
-      familyNames: null,
-      institution: 'Public Education, Public Affiliation',
-    },
-    {
-      orcid: '0000-0003-0625-2121',
-      email: null,
-      givenNames: 'Affiliations',
-      familyNames: null,
-      institution: 'Public Education, Public Affiliation',
-    },
-    {
-      orcid: '0000-0003-0625-2121',
-      email: null,
-      givenNames: 'Affiliations',
-      familyNames: null,
-      institution: 'Public Education, Public Affiliation',
-    },
-  ]
-
   constructor(private dialog: MatDialog) {}
 
-  ngOnInit() {
-    this.dialog.open(IsThisYouComponent, {
-      width: `1078px`,
-      data: {
-        duplicateRecords: this.duplicateRecords,
-        titleLabel: 'MY OWN TITTLE',
-      },
-    })
-  }
+  ngOnInit() {}
 }
--- src/app/sign-in/sign-in.module.ts
+++ src/app/sign-in/sign-in.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { SignInComponent } from './pages/sign-in/sign-in.component'
-import { IsThisYouModule } from '../cdk/is-this-you/is-this-you.module'
 import { SignInRoutingModule } from './sign-in-routing.module'
 import { MatDialogModule } from '@angular/material'
-import { IsThisYouComponent } from '../cdk/is-this-you/is-this-you.component'
 
 @NgModule({
   declarations: [SignInComponent],
-  imports: [
-    CommonModule,
-    IsThisYouModule,
-    SignInRoutingModule,
-    MatDialogModule,
-  ],
-  entryComponents: [IsThisYouComponent],
+  imports: [CommonModule, SignInRoutingModule, MatDialogModule],
+  entryComponents: [],
 })
 export class SignInModule {}
