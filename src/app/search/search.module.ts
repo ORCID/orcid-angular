8d76a50092d34366c1ffd366e24d7c6783b8c50a
leomendoza123 <leomendoza123@gmail.com>
2020-01-21 17:46:41 -0600

    Add basic results HTML component

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -4,9 +4,20 @@ import { CommonModule } from '@angular/common'
 import { SearchRoutingModule } from './search-routing.module'
 import { SearchComponent } from './pages/search/search.component'
 import { ResultsComponent } from './components/results/results.component'
+import {
+  MatPaginatorModule,
+  MatButtonModule,
+  MatIconModule,
+} from '@angular/material'
 
 @NgModule({
   declarations: [SearchComponent, ResultsComponent],
-  imports: [CommonModule, SearchRoutingModule],
+  imports: [
+    CommonModule,
+    SearchRoutingModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class SearchModule {}
