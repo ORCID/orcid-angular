e44fba9d59f16cabf5cd7a01aef8f68a7deb9169
leomendoza123 <leomendoza123@gmail.com>
2020-01-22 11:56:58 -0600

    Add styles

--- src/app/search/components/results/results.component.ts
+++ src/app/search/components/results/results.component.ts
@@ -4,9 +4,9 @@ import { Component, OnInit } from '@angular/core'
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss'],
+  preserveWhitespaces: true,
 })
 export class ResultsComponent implements OnInit {
   constructor() {}
-
   ngOnInit() {}
 }
