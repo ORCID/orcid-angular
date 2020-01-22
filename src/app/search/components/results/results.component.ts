c3c801564f86b78270c5da8b93b45c438b385fa8
leomendoza123 <leomendoza123@gmail.com>
2020-01-22 12:46:27 -0600

    Create a paginator overwrite and result theme

--- src/app/search/components/results/results.component.ts
+++ src/app/search/components/results/results.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core'
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss'],
+  styleUrls: [
+    './results.component.scss-theme.scss',
+    './results.component.scss',
+  ],
   preserveWhitespaces: true,
 })
 export class ResultsComponent implements OnInit {
