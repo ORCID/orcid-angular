4df9c03ea83b3e797d936575f00f8ce872951310
leomendoza123 <leomendoza123@gmail.com>
2020-02-06 17:57:05 -0600

    Add search types

---
+++ src/app/types/search.local.ts
@@ -0,0 +1,11 @@
+export interface SearchParameters {
+  searchQuery?: string
+  firstName?: string
+  lastName?: string
+  institution?: string
+  keyword?: string
+  otherFields?: string
+  orcid?: string
+  resultsOffset?: number
+  pageSize?: number
+}
