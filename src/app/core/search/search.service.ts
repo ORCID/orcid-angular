3c427e4afd86a93211d32872be2b21f5e0047829
leomendoza123 <leomendoza123@gmail.com>
2020-02-10 16:23:28 -0600

    Delete unused import

--- src/app/core/search/search.service.ts
+++ src/app/core/search/search.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core'
-import { Params } from '@angular/router'
-import { environment } from '../../../environments/environment'
+import { environment } from 'src/environments/environment'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { SearchParameters, SearchResults } from 'src/app/types'
