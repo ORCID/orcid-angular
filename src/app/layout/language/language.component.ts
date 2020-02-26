999f70f88b57fe157e8635bd54f3a9a62b5ae0e7
leomendoza123 <leomendoza123@gmail.com>
2020-02-26 14:19:13 -0600

    Fix language on deep links

--- src/app/layout/language/language.component.ts
+++ src/app/layout/language/language.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject, LOCALE_ID } from '@angular/core'
 import { environment } from 'src/environments/environment'
 import { WINDOW } from 'src/app/cdk/window'
 import { CookieService } from 'ngx-cookie-service'
+import { TogglzService } from 'src/app/core/togglz/togglz.service'
+import { LanguageService } from 'src/app/core/language/language.service'
 
 @Component({
   selector: 'app-language',
@@ -10,21 +12,35 @@ import { CookieService } from 'ngx-cookie-service'
 })
 export class LanguageComponent implements OnInit {
   languageMenuOptions
+  changeLanguageWithoutExtraBackendCallToggl = true
   constructor(
     @Inject(LOCALE_ID) public locale: string,
     @Inject(WINDOW) private window: Window,
-    private _cs: CookieService
+    private _cs: CookieService,
+    private _togglz: TogglzService,
+    private _language: LanguageService
   ) {
     this.languageMenuOptions = environment.LANGUAGE_MENU_OPTIONS
+    this._togglz
+      .getStateOf('CHANGE_LANGUAGE_WITHOUT_EXTRA_BACKEND_CALL')
+      .subscribe(
+        value => (this.changeLanguageWithoutExtraBackendCallToggl = value)
+      )
   }
 
+  // If the togglz feature CHANGE_LANGUAGE_WITHOUT_EXTRA_BACKEND_CALL is enable
+  // the cookie will be updated locally and the page will be reloaded
+  // if not a backend call and the reload will be executed
   changeLanguage(languageKey: string) {
-    if (environment.production) {
-      this._cs.set('locale_v3', languageKey)
+    if (this.changeLanguageWithoutExtraBackendCallToggl) {
+      this._cs.set('locale_v3', languageKey, null, '/')
       this.window.location.reload()
     } else {
-      this.window.location.href = '/' + languageKey + '/'
+      this._language.changeLanguage(languageKey).subscribe(() => {
+        this.window.location.reload()
+      })
     }
   }
+
   ngOnInit() {}
 }
