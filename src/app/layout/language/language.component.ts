cd257c61c29a256a867854b15015b3fc739b7569
leomendoza123 <leomendoza123@gmail.com>
2020-02-26 15:09:10 -0600

    Remove unrequired imports

--- src/app/layout/language/language.component.ts
+++ src/app/layout/language/language.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Inject, LOCALE_ID } from '@angular/core'
 import { environment } from 'src/environments/environment'
 import { WINDOW } from 'src/app/cdk/window'
-import { CookieService } from 'ngx-cookie-service'
-import { TogglzService } from 'src/app/core/togglz/togglz.service'
 import { LanguageService } from 'src/app/core/language/language.service'
 
 @Component({
@@ -15,15 +13,11 @@ export class LanguageComponent implements OnInit {
   constructor(
     @Inject(LOCALE_ID) public locale: string,
     @Inject(WINDOW) private window: Window,
-    private _cs: CookieService,
     private _language: LanguageService
   ) {
     this.languageMenuOptions = environment.LANGUAGE_MENU_OPTIONS
   }
 
-  // If the togglz feature CHANGE_LANGUAGE_WITHOUT_EXTRA_BACKEND_CALL is enable
-  // the cookie will be updated locally and the page will be reloaded
-  // if not a backend call and the reload will be executed
   changeLanguage(languageKey: string) {
     this._language.changeLanguage(languageKey).subscribe(() => {
       this.window.location.reload()
