8d0e472413aca5220c4bd28a32aa6764f5791431
leomendoza123 <leomendoza123@gmail.com>
2020-03-17 15:19:28 -0600

    Add closable privacy policy banner

--- src/app/layout/maintenance-message/maintenance-message.component.ts
+++ src/app/layout/maintenance-message/maintenance-message.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { TogglzService } from 'src/app/core/togglz/togglz.service'
+import { CookieService } from 'ngx-cookie-service'
 
 @Component({
   selector: 'app-maintenance-message',
@@ -11,10 +12,41 @@ import { TogglzService } from 'src/app/core/togglz/togglz.service'
 })
 export class MaintenanceMessageComponent implements OnInit {
   maintenanceMessage
-  constructor(togglz: TogglzService) {
+  closableElements: NodeListOf<Element>
+  closableElement: Element
+  nonClosableElements: NodeListOf<Element>
+  constructor(togglz: TogglzService, private _cookie: CookieService) {
     togglz.getMessageOf('MAINTENANCE_MESSAGE').subscribe(value => {
       this.maintenanceMessage = value
+      const parser = new DOMParser()
+      const htmlElement = parser.parseFromString(
+        this.maintenanceMessage,
+        'text/html'
+      )
+      this.closableElements = htmlElement.querySelectorAll('div.closable')
+      this.nonClosableElements = htmlElement.querySelectorAll('div.regular')
+      this.updateClosableMessage()
     })
   }
+
+  updateClosableMessage() {
+    this.closableElements.forEach(node => {
+      if (
+        node &&
+        node.id &&
+        !this._cookie.check(node.id) &&
+        !this.closableElement
+      ) {
+        this.closableElement = node
+      }
+    })
+  }
+
+  understoodClosableMessage(element: Element) {
+    this.closableElement = null
+    this._cookie.set(element.id, 'understood', 365)
+    this.updateClosableMessage()
+  }
+
   ngOnInit() {}
 }
