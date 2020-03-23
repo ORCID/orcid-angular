6e2482b02afff75b2d179e251c0cc4f8b318e990
Leonardo Mendoza Fernández <leomendoza123@gmail.com>
2020-03-23 14:58:47 -0600

    Add hotjar and postbuild script

---
+++ scripts/hotjar-analytics.postbuild.ts
@@ -0,0 +1,17 @@
+export function hotjarAnalytics(indexHtml: string, options) {
+  if (
+    options.environmentVariables &&
+    options.environmentVariables.HOTJAR_ANALYTICS
+  ) {
+    console.log(indexHtml)
+    return indexHtml.replace(
+      /hjid: 0000000/g,
+      options.environmentVariables.HOTJAR_ANALYTICS
+    )
+  } else {
+    console.warn(
+      'This build will not have hotjar analytics since the hjid is not defined on the environment variables'
+    )
+    return indexHtml
+  }
+}
