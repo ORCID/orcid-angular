cfa9eaf33fe4ec52d81f774f826085428e98ee7c
leomendoza123 <leomendoza123@gmail.com>
2020-03-31 12:07:42 -0600

    Add local validations and add getMaintenance parser as a service

--- src/app/layout/banners/banners.component.ts
+++ src/app/layout/banners/banners.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { CookieService } from 'ngx-cookie-service'
 import { PlatformInfoService } from 'src/app/cdk/platform-info'
 import { take } from 'rxjs/operators'
+import { TogglzService } from 'src/app/core/togglz/togglz.service'
 
 @Component({
   selector: 'app-banners',
@@ -10,9 +11,20 @@ import { take } from 'rxjs/operators'
   preserveWhitespaces: true,
 })
 export class BannersComponent implements OnInit {
+  maintenanceMessages
   showCookieBanner
   showUnsupportedBrowserBanner
-  constructor(_cookie: CookieService, _platformInfo: PlatformInfoService) {
+  closableElementAtDisplay
+  constructor(
+    private _cookie: CookieService,
+    _platformInfo: PlatformInfoService,
+    togglz: TogglzService
+  ) {
+    togglz.getMaintenanceMessages().subscribe(value => {
+      this.maintenanceMessages = value
+      this.updateClosableMessage()
+    })
+
     _platformInfo
       .get()
       .pipe(take(1))
@@ -21,6 +33,27 @@ export class BannersComponent implements OnInit {
           (this.showUnsupportedBrowserBanner = platform.unsupportedBrowser)
       )
     this.showCookieBanner = !_cookie.check('orcidCookiePolicyAlert')
+  }
+
+  updateClosableMessage() {
+    if (this.maintenanceMessages && this.maintenanceMessages.closableElements) {
+      this.maintenanceMessages.closableElements.forEach(node => {
+        if (
+          node &&
+          node.id &&
+          !this._cookie.check(node.id) &&
+          !this.closableElementAtDisplay
+        ) {
+          this.closableElementAtDisplay = node
+        }
+      })
+    }
+  }
+
+  understoodClosableMessage(element: Element) {
+    this.closableElementAtDisplay = null
+    this._cookie.set(element.id, 'understood', 365)
+    this.updateClosableMessage()
   }
 
   ngOnInit() {}
