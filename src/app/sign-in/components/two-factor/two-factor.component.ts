38514c7f6cadf2f4535f128a5ccdcf5efb72fd3d
Daniel Palafox <dp852@hotmail.com>
2020-04-17 11:47:57 -0500

    Remove environment localhost and unused import

--- src/app/sign-in/components/two-factor/two-factor.component.ts
+++ src/app/sign-in/components/two-factor/two-factor.component.ts
@@ -10,7 +10,6 @@ import {
   ViewChild,
 } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
-import { environment } from '../../../../environments/environment.local'
 import { WINDOW } from '../../../cdk/window'
 
 @Component({
