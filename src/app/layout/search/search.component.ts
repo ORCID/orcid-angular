315485a90da639fc44e91aad3451e96f543a82fb
Daniel Palafox <dp852@hotmail.com>
2020-06-25 17:10:15 -0500

    add new search property

--- src/app/layout/search/search.component.ts
+++ src/app/layout/search/search.component.ts
@@ -55,9 +55,14 @@ export class SearchComponent implements OnInit {
     _togglz
       .getStateOf('ORCID_ANGULAR_SEARCH')
       .subscribe((value) => (this.togglzOrcidAngularSearch = value))
-    _togglz
-      .getStateOf('NEW_INFO_SITE')
-      .subscribe((value) => (this.togglzNewInfoSite = value))
+    _togglz.getStateOf('NEW_INFO_SITE').subscribe((value) => {
+      this.togglzNewInfoSite = value
+      if (!this.togglzNewInfoSite) {
+        this.searchPlaceHolder = this.firstLetterUppercase(
+          $localize`:@@ngOrcid.searchNewInfo:Search the ORCID registry`
+        )
+      }
+    })
 
     router.events.subscribe(
       () =>
