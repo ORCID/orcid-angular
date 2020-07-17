8dd12f405791329db278d4e3d6c7c8fc21ef38ad
leomendoza123 <leomendoza123@gmail.com>
2020-07-16 19:10:15 -0600

    Workarround angular issue related with stausChange method

--- src/app/register/components/BaseForm.ts
+++ src/app/register/components/BaseForm.ts
@@ -6,7 +6,7 @@ import {
   ValidationErrors,
   AsyncValidator,
 } from '@angular/forms'
-import { of, Observable, timer } from 'rxjs'
+import { of, Observable, timer, merge } from 'rxjs'
 import { filter, switchMap, map, tap, take, startWith } from 'rxjs/operators'
 
 export abstract class BaseForm implements ControlValueAccessor, AsyncValidator {
@@ -30,7 +30,18 @@ export abstract class BaseForm implements ControlValueAccessor, AsyncValidator {
     isDisabled ? this.form.disable() : this.form.enable()
   }
   validate(c: AbstractControl): Observable<ValidationErrors | null> {
-    return this.form.statusChanges.pipe(
+    // temporal fix
+    // see related issue
+    // https://github.com/angular/angular/issues/14542
+    // depending of fix
+    // https://github.com/angular/angular/pull/20806
+    //
+    // using only form.statusChanges observable only would be a better solution for this scenario (see the code before this fix)
+    // but if the form status starts as pending Angular wont report the change because of #14542
+    // and the status can now start as pending with the introduction of values on a Oauth registration thought query parameters
+
+    return merge(this.form.statusChanges, timer(0, 1000)).pipe(
+      map(() => this.form.status),
       startWith(this.form.status),
       filter((value) => value !== 'PENDING'),
       take(1),
