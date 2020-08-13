7451cc024948d867907d06b291fdbb7bf0e6b5c0
leomendoza123 <leomendoza123@gmail.com>
2020-08-13 13:03:42 -0600

    Update errors docs

--- src/app/errors.ts
+++ src/app/errors.ts
@@ -7,6 +7,8 @@ export const ERROR_REPORT: {
   RESET_PASSWORD_COULD_NOT_RECOVER: ErrorReport
   REGISTER: ErrorReport
 } = {
+  // The JUST_GA error
+  // It will report the error only on google analytics
   JUST_GA: {
     analytics: {
       code: '000',
@@ -14,13 +16,16 @@ export const ERROR_REPORT: {
     },
   },
 
+  // The STANDARD
+  // use by default on every not expect HTTP response
+  // It will report the error on google analytics and will be show to the user **only on QA and local dev environments**
   STANDARD: {
     display: {
       title: $localize`:@@ngOrcid.error:Oh no! An error occurred!`,
       message: `not verbose`,
       displayOnlyOnVerboseEnvironment: true,
-      action: $localize`:@@shared.contactSupport:contact support.`,
-      actionURL: `https://support.orcid.org/hc/en-us/requests/new`,
+      action: $localize`:@@shared.knowledgeBase:visit our knowledge base.`,
+      actionURL: `https://support.orcid.org/hc/en-us/articles/360047687054`,
     },
     analytics: {
       code: '001',
@@ -28,9 +33,12 @@ export const ERROR_REPORT: {
     },
   },
 
+  // The STANDARD_VERBOSE
+  // It will report the error on google analytics and the snackbar will be show to the user
   STANDARD_VERBOSE: {
     display: {
       title: $localize`:@@ngOrcid.error:Oh no! An error occurred!`,
+      message: $localize`:@@shared.standardError:Please try again, and if the error persists please`,
       action: $localize`:@@shared.knowledgeBase:visit our knowledge base.`,
       actionURL: `https://support.orcid.org/hc/en-us/articles/360047687054`,
     },
@@ -40,6 +48,8 @@ export const ERROR_REPORT: {
     },
   },
 
+  // The RESET_PASSWORD_COULD_NOT_RECOVER is used only on the /reset-password page after the user clicks the recover account details button
+  // It will report the error on google analytics and the snackbar will be show to the user
   RESET_PASSWORD_COULD_NOT_RECOVER: {
     display: {
       title: $localize`:@@ngOrcid.error:Oh no! An error occurred.`,
@@ -54,6 +64,8 @@ export const ERROR_REPORT: {
     },
   },
 
+  // The REGISTER is used only on the /register page after the user clicks the register button
+  // It will report the error on google analytics and the snackbar will be show to the user
   REGISTER: {
     display: {
       title: $localize`:@@ngOrcid.error:Oh no! An error occurred.`,
