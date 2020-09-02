5cb5abaa9fd7eba57f9741c22ceaa0e42742ad43
leomendoza123 <leomendoza123@gmail.com>
2020-09-02 15:14:06 -0600

    Add panel styling

--- src/app/cdk/pannel/panel/panel.component.ts
+++ src/app/cdk/pannel/panel/panel.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit, Input } from '@angular/core'
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
-  styleUrls: ['./panel.component.scss'],
+  styleUrls: ['./panel.component.scss', 'panel.component.scss-theme.scss'],
 })
 export class PanelComponent implements OnInit {
+  openState = false
   constructor() {}
 
   ngOnInit(): void {}
