5c7b53de57deddd5a5b562d8eade3b548bb18bfa
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2020-09-23 13:17:52 -0600

    Improve management on language guard

--- src/app/guards/language.guard.ts
+++ src/app/guards/language.guard.ts
@@ -1,18 +1,19 @@
 import { Inject, Injectable, LOCALE_ID } from '@angular/core'
 import {
   ActivatedRouteSnapshot,
-  RouterStateSnapshot,
   CanActivateChild,
   Router,
-  UrlTree,
+  RouterStateSnapshot,
 } from '@angular/router'
 import { CookieService } from 'ngx-cookie-service'
 import { NEVER, Observable, of } from 'rxjs'
-import { catchError, first, map, switchMap, tap } from 'rxjs/operators'
+import { catchError, first, switchMap, tap } from 'rxjs/operators'
+
 import { PlatformInfoService } from '../cdk/platform-info'
 import { WINDOW } from '../cdk/window'
 import { ErrorHandlerService } from '../core/error-handler/error-handler.service'
 import { LanguageService } from '../core/language/language.service'
+import { LanguageContext } from '../types/language.locale'
 
 @Injectable({
   providedIn: 'root',
@@ -30,12 +31,10 @@ export class LanguageGuard implements CanActivateChild {
   canActivateChild(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> {
+  ): Observable<boolean> {
     const queryParams = next.queryParams
-    const parameterLanguage = this.normalizeLanguageCode(
-      queryParams['lang'] || queryParams['LANG']
-    )
-    const angularAppLocaleCode = this.normalizeLanguageCode(this.locale)
+
+    let langContext: LanguageContext = this.getLanguageContext(queryParams)
 
     return this._platform.get().pipe(
       first(),
@@ -44,24 +43,52 @@ export class LanguageGuard implements CanActivateChild {
           // tslint:disable-next-line: max-line-length
           Object.keys(queryParams).length && // TODO @leomendoza123 make the platform handle the query parameters of the guards correctly, this line is a temporary quick fix
           platform.oauthMode &&
-          parameterLanguage &&
-          angularAppLocaleCode.indexOf(parameterLanguage) === -1 &&
-          !this.hasCloudflareEtagIssue()
+          this.requireLanguageCookieUpdate(langContext)
         ) {
-          return this._language.changeLanguage(parameterLanguage).pipe(
-            map((value) => this.window.location.reload()),
-            switchMap((value) => NEVER),
-            catchError((value) => of(true)) // When the language change fail ignore and continue
+          return this._language.changeLanguage(langContext.param).pipe(
+            tap(() => {
+              // refresh language context after the language change
+              langContext = this.getLanguageContext(queryParams)
+            }),
+            switchMap(() => {
+              // The browser might be already loading the right language thanks to the query parameter and local cache
+              // so a reload is not require
+              if (this.currentAppLanguageMatchTheCookieLanguage(langContext)) {
+                return of(true)
+              } else {
+                // the browser needs to be reloaded to load the correct language
+                return of(this.window.location.reload()).pipe(
+                  switchMap(() => NEVER)
+                )
+              }
+            }),
+            catchError((value) => of(true)) // If the language change endpoint fail ignore and continue
           )
-        } else if (this.hasCloudflareEtagIssue()) {
+        } else if (
+          !this.currentAppLanguageMatchTheCookieLanguage(langContext)
+        ) {
+          /**
+           * If on the application load the language cookie (set by the backend) and the real app language differ
+           * it is highly likely an issue with cloudflare
+           */
           return this._errorHandler
-            .handleError(new Error('etagIssueDetected/'))
+            .handleError(new Error('cloudflareIssueDetected/'))
             .pipe(catchError((error) => of(true)))
         } else {
           return of(true)
         }
       })
     )
+  }
+
+  getLanguageContext(queryParams): LanguageContext {
+    return {
+      param: this.normalizeLanguageCode(
+        queryParams['lang'] || queryParams['LANG']
+      ),
+      app: this.normalizeLanguageCode(this.locale),
+      cookie: this.normalizeLanguageCode(this._cookies.get('locale_v3')),
+    }
   }
 
   /**
@@ -77,24 +104,18 @@ export class LanguageGuard implements CanActivateChild {
     return languageCode
   }
 
-  /**
-   * If on the application load the language cookie (set by the backend) and the real app language differ
-   * it is highly likely an Etag issue with cloudflare
-   */
-  hasCloudflareEtagIssue() {
-    const localeCookie = this.normalizeLanguageCode(
-      this._cookies.get('locale_v3')
-    )
-    const angularAppLocaleCode = this.normalizeLanguageCode(this.locale)
-
+  currentAppLanguageMatchTheCookieLanguage(langContext: LanguageContext) {
     if (
-      angularAppLocaleCode &&
-      localeCookie &&
-      angularAppLocaleCode !== localeCookie
+      langContext.cookie &&
+      langContext.app &&
+      langContext.app.indexOf(langContext.cookie) === -1
     ) {
-      return true
-    } else {
       return false
     }
+    return true
+  }
+
+  requireLanguageCookieUpdate(lang: LanguageContext): boolean {
+    return lang.param && lang.cookie && lang.cookie.indexOf(lang.param) === -1
   }
 }
