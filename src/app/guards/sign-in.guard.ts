40fc907c745ec73aabbbe88e3a89a571ab54d8a2
Daniel Palafox <dp852@hotmail.com>
2020-09-23 15:21:27 -0500

    fix ignore show_login

--- src/app/guards/sign-in.guard.ts
+++ src/app/guards/sign-in.guard.ts
@@ -46,18 +46,17 @@ export class SignInGuard implements CanActivateChild {
   }
 
   handleOauthSession(queryParams: OauthParameters) {
-    // If the show login parameters is present redirect the user to the register
-    if (queryParams.show_login === 'false') {
-      return of(
-        this._router.createUrlTree(['/register'], {
-          queryParams: queryParams,
-        })
-      )
-    }
     // check if the user is already login or there are errors
     return this._user.getUserSession(queryParams).pipe(
       map((session) => {
         const oauthSession = session.oauthSession
+        // If the show login parameters is present redirect the user to the register
+        if (queryParams.show_login && (queryParams.email || queryParams.orcid) &&  !session.oauthSession.userId) {
+          return this.redirectToRegister(queryParams)
+        } else if (queryParams.show_login === 'false' && !session.oauthSession.userId) {
+          return this.redirectToRegister(queryParams)
+        }
+
         if (
           oauthSession &&
           !oauthSession.forceLogin &&
@@ -70,5 +69,11 @@ export class SignInGuard implements CanActivateChild {
         return true
       })
     )
+  }
+
+  private redirectToRegister(queryParams) {
+    return this._router.createUrlTree(['/register'], {
+      queryParams: queryParams,
+    })
   }
 }
