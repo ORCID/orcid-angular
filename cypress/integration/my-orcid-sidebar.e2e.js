02f7e25cac56b9444abaa22396c23f00202d67b2
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2020-11-13 10:59:44 -0600

    Sidebar expectations

---
+++ cypress/integration/my-orcid-sidebar.e2e.js
@@ -0,0 +1,116 @@
+describe('My Orcid sidebar' + runInfo(), () => {
+  describe('Emails' + runInfo(), () => {
+    it('display a user with only a primary email "unverified"', () => {})
+    it('display a user with only a primary email "verified"', () => {})
+    it('add additional emails and display those as "unverified" with default privacy', () => {
+      // Expect changes to be display outside and inside of the modal
+      // TODO @dandineen no sketch that displays the input box
+    })
+    it('remove/delete  emails and display those as "unverified" with default privacy', () => {
+      // Expect changes to be display outside and inside of the modal
+    })
+    it('display emails "unverified" and "verified"', () => {
+      // Expect changes to be display outside and inside of the modal
+    })
+    it('resend the email verification email', () => {
+      // TODO @dandineen no button to resend
+    })
+    it('set a "checked" email as primary', () => {
+      // checks that a secondary email does not have the `make primary` button
+      // Expect changes to be display outside and inside of the modal
+    })
+    it('change the email privacy', () => {})
+    it('make changes and cancel', () => {
+      // Expect changes NOT to be display outside and inside of the modal
+    })
+    it('clicks outside and DONT close the modal ', () => {})
+    it('open the terms of use on a separate window ', () => {})
+  })
+
+  describe('Websites and socials links', () => {
+    it('display a user with no items', () => {
+      // TODO @dandineen should this look just empty on the modal
+    })
+    it('add items and display those with default privacy', () => {
+      // Expect changes to be display outside and inside of the modal
+      // TODO @dandineen no sketch that displays the input box
+    })
+    it('remove/delete', () => {
+      // Expect changes to be display outside and inside of the modal
+    })
+    it('Drag and drop to rearrange', () => {
+      // Expect changes to be display outside and inside of the modal
+    })
+    it('change privacy', () => {
+      // Expect changes to be display outside and inside of the modal
+    })
+    it('make changes and cancel', () => {
+      // Expect changes NOT to be display outside and inside of the modal
+    })
+    it('clicks outside and DONT close the modal ', () => {})
+  })
+  describe('Countries', () => {
+    it('display a user with no items', () => {
+      // TODO @dandineen should this look just empty on the modal
+    })
+    it('add items and display those with default privacy', () => {
+      // Expect changes to be display outside and inside of the modal
+      // TODO @dandineen no sketch that displays the input box
+    })
+    it('remove/delete', () => {
+      // Expect changes to be display outside and inside of the modal
+    })
+    it('Drag and drop to rearrange', () => {
+      // Expect changes to be display outside and inside of the modal
+    })
+    it('change privacy', () => {
+      // Expect changes to be display outside and inside of the modal
+    })
+    it('make changes and cancel', () => {
+      // Expect changes NOT to be display outside and inside of the modal
+    })
+    it('clicks outside and DONT close the modal ', () => {})
+  })
+  describe('Other IDs', () => {
+    it('display a user with no items', () => {
+      // TODO @dandineen should this look just empty on the modal
+    })
+    it('display a user with items and default privacy', () => {})
+    it('remove/delete', () => {
+      // Expect changes to be display outside and inside of the modal
+    })
+    it('Drag and drop to rearrange', () => {
+      // Expect changes to be display outside and inside of the modal
+    })
+    it('change privacy', () => {
+      // Expect changes to be display outside and inside of the modal
+    })
+    it('make changes and cancel', () => {
+      // Expect changes NOT to be display outside and inside of the modal
+    })
+    it('clicks outside and DONT close the modal ', () => {})
+  })
+  describe('Keywords', () => {
+    it('display a user with no items', () => {
+        // TODO @dandineen should this look just empty on the modal
+      })
+      it('add items and display those with default privacy', () => {
+        // Expect changes to be display outside and inside of the modal
+        // TODO @dandineen no sketch that displays the input box
+      })
+      it('remove/delete', () => {
+        // Expect changes to be display outside and inside of the modal
+      })
+      it('Drag and drop to rearrange', () => {
+        // Expect changes to be display outside and inside of the modal
+      })
+      it('change privacy', () => {
+        // Expect changes to be display outside and inside of the modal
+      })
+      it('make changes and cancel', () => {
+        // Expect changes NOT to be display outside and inside of the modal
+      })
+      it('clicks outside and DONT close the modal ', () => {})
+
+  })
+})
