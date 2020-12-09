42bd6e30ad35a46ad8107a0a4c70a4bee13746dc
Daniel Palafox <dp852@hotmail.com>
2020-12-08 19:50:24 -0600

    fix oauth with the word social in the redirect uri

--- src/app/cdk/platform-info/platform-info.service.ts
+++ src/app/cdk/platform-info/platform-info.service.ts
@@ -213,9 +213,10 @@ export class PlatformInfoService {
     this.platform = {
       ...this.platform,
       hasOauthParameters: this.hasOauthParameters(),
-      social: this.window.location.href.toLowerCase().indexOf('social') >= 0,
+      social: this.window.location.pathname.toLowerCase().indexOf('social-linking') >= 0,
       institutional:
-        this.window.location.href.toLowerCase().indexOf('institutional') >= 0,
+        this.window.location.pathname.toLowerCase().indexOf('institutional-linking') >= 0 ||
+        this.window.location.pathname.toLowerCase().indexOf('institutional-signin') >= 0,
     }
     this.platformSubject.next(this.platform)
     return this.platformSubject.asObservable()
