31377d53b877b3dc34bf1847c7fdc353cd52f329
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2021-01-07 15:54:59 -0600

    Add grid structure

--- src/app/cdk/modal/modal.module.ts
+++ src/app/cdk/modal/modal.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ModalComponent } from './modal/modal.component';
-import { ModalHeaderComponent } from './modal-header/modal-header.component';
-import { ModalFooterComponent } from './modal-footer/modal-footer.component';
-import { ModalSideBarComponent } from './modal-side-bar/modal-side-bar.component';
-
-
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { ModalComponent } from './modal/modal.component'
+import { ModalHeaderComponent } from './modal-header/modal-header.component'
+import { ModalFooterComponent } from './modal-footer/modal-footer.component'
+import { ModalSideBarComponent } from './modal-side-bar/modal-side-bar.component'
 
 @NgModule({
-  declarations: [ModalComponent, ModalHeaderComponent, ModalFooterComponent, ModalSideBarComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    ModalComponent,
+    ModalHeaderComponent,
+    ModalFooterComponent,
+    ModalSideBarComponent,
+  ],
+  imports: [CommonModule],
+  exports: [
+    ModalComponent,
+    ModalHeaderComponent,
+    ModalFooterComponent,
+    ModalSideBarComponent,
+  ],
 })
-export class ModalModule { }
+export class ModalModule {}
--- src/app/cdk/side-bar/side-bar.module.ts
+++ src/app/cdk/side-bar/side-bar.module.ts
@@ -4,10 +4,11 @@ import { SideBarComponent } from './side-bar/side-bar.component'
 import { SideBarIdComponent } from './side-bar-id/side-bar-id.component'
 import { PannelModule } from '../pannel/pannel.module';
 import { AlsoKnowAsComponent } from './modals/also-know-as/also-know-as.component'
+import { ModalModule } from '../modal/modal.module';
 
 @NgModule({
   declarations: [SideBarComponent, SideBarIdComponent, AlsoKnowAsComponent],
-  imports: [CommonModule, PannelModule],
+  imports: [CommonModule, PannelModule, ModalModule],
   exports: [SideBarComponent],
 })
 export class SideBarModule {}
