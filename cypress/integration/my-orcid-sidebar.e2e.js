b3e3aa72e50c6f93c1f290a3be67bdb48083769e
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2021-01-15 15:05:58 -0600

    Improve expects

--- cypress/integration/my-orcid-sidebar.e2e.js
+++ cypress/integration/my-orcid-sidebar.e2e.js
@@ -6,37 +6,43 @@ describe('My Orcid sidebar' + runInfo(), () => {
   describe('Emails' + runInfo(), () => {
     it('display a user with only a primary email "unverified"', () => {})
     it('display a user with only a primary email "verified"', () => {})
-    it('add additional emails and display those as "unverified" with default privacy', () => {
-      // Expect changes to be display outside and inside of the modal
-      // TODO @dandineen no sketch that displays the input box
-    })
-    it('remove/delete  emails and display those as "unverified" with default privacy', () => {
+    it('display multiple emails "unverified" and "verified"', () => {
       // Expect changes to be display outside and inside of the modal
     })
-    it('display emails "unverified" and "verified"', () => {
-      // Expect changes to be display outside and inside of the modal
+    describe('Emails edit modals' + runInfo(), () => {
+      it('add additional emails and display those as "unverified" with default privacy', () => {
+        // Expect changes to be display outside and inside of the modal
+      })
+      it('remove emails', () => {
+        // Expect the primary email to not display the delete button
+        // Expect changes to be display outside and inside of the modal
+      })
+      it('edit a non-primary and display it as "unverified"', () => {
+        // Expect changes to be display outside and inside of the modal
+      })
+      it('edit a primary email and display it as "verified"', () => {
+        // Expect changes to be display outside and inside of the modal
+      })
+      it('display multiple emails "unverified" and "verified"', () => {
+        // Expect changes to be display outside and inside of the modal
+      })
+      it('resend the email verification email', () => {})
+      it('set a "verified" email as primary', () => {
+        // checks that a secondary email does not have the `make primary` button if not verified
+        // Expect changes to be display outside and inside of the modal
+      })
+      it('change the email privacy', () => {})
+      it('make changes and cancel', () => {
+        // Expect changes NOT to be display outside and inside of the modal
+      })
+      it('clicks outside and DONT close the modal ', () => {})
+      it('open the terms of use on a separate window ', () => {})
     })
-    it('resend the email verification email', () => {
-      // TODO @dandineen no button to resend
-    })
-    it('set a "checked" email as primary', () => {
-      // checks that a secondary email does not have the `make primary` button
-      // Expect changes to be display outside and inside of the modal
-    })
-    it('change the email privacy', () => {})
-    it('make changes and cancel', () => {
-      // Expect changes NOT to be display outside and inside of the modal
-    })
-    it('clicks outside and DONT close the modal ', () => {})
-    it('open the terms of use on a separate window ', () => {})
   })
   describe('Websites and socials links', () => {
-    it('display a user with no items', () => {
-      // TODO @dandineen should this look just empty on the modal
-    })
+    it('display a user with no items', () => {})
     it('add items and display those with default privacy', () => {
       // Expect changes to be display outside and inside of the modal
-      // TODO @dandineen no sketch that displays the input box
     })
     it('remove/delete', () => {
       // Expect changes to be display outside and inside of the modal
@@ -53,12 +59,9 @@ describe('My Orcid sidebar' + runInfo(), () => {
     it('clicks outside and DONT close the modal ', () => {})
   })
   describe('Countries', () => {
-    it('display a user with no items', () => {
-      // TODO @dandineen should this look just empty on the modal
-    })
+    it('display a user with no items', () => {})
     it('add items and display those with default privacy', () => {
       // Expect changes to be display outside and inside of the modal
-      // TODO @dandineen no sketch that displays the input box
     })
     it('remove/delete', () => {
       // Expect changes to be display outside and inside of the modal
@@ -77,7 +80,6 @@ describe('My Orcid sidebar' + runInfo(), () => {
   describe('Other IDs', () => {
     it('display a user with no items', () => {
       // should be hidden from the side bar https://trello.com/c/JZJ75TWl/35-how-to-add-other-ids-is-confusing
-      // TODO @dandineen should this look just empty on the modal
     })
     it('display a user with items and default privacy', () => {})
     it('remove/delete', () => {
@@ -95,12 +97,9 @@ describe('My Orcid sidebar' + runInfo(), () => {
     it('clicks outside and DONT close the modal ', () => {})
   })
   describe('Keywords', () => {
-    it('display a user with no items', () => {
-      // TODO @dandineen should this look just empty on the modal
-    })
+    it('display a user with no items', () => {})
     it('add items and display those with default privacy', () => {
       // Expect changes to be display outside and inside of the modal
-      // TODO @dandineen no sketch that displays the input box
     })
     it('remove/delete', () => {
       // Expect changes to be display outside and inside of the modal
