bb1493570f25f418899c0c414f588377ae03c7cf
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2021-02-04 11:46:25 -0600

    Improve id wrapping

--- src/app/cdk/side-bar/side-bar-id/side-bar-id.component.ts
+++ src/app/cdk/side-bar/side-bar-id/side-bar-id.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core'
+import { Component, OnInit, Input, OnDestroy } from '@angular/core'
+import { Subject } from 'rxjs/internal/Subject'
+import { first, takeUntil } from 'rxjs/operators'
+import { PlatformInfoService } from '../../platform-info'
 
 @Component({
   selector: 'app-side-bar-id',
@@ -8,10 +11,25 @@ import { Component, OnInit, Input } from '@angular/core'
     './side-bar-id.component-scss-theme.scss',
   ],
 })
-export class SideBarIdComponent implements OnInit {
+export class SideBarIdComponent implements OnInit, OnDestroy {
+  $destroy: Subject<boolean> = new Subject<boolean>()
+
+  desktop: boolean
   @Input() id: string
   privateView = true
-  constructor() {}
+  constructor(private _platform: PlatformInfoService) {
+    this._platform
+      .get()
+      .pipe(takeUntil(this.$destroy))
+      .subscribe((x) => {
+        this.desktop = x.columns12
+      })
+  }
 
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.$destroy.next(true)
+    this.$destroy.unsubscribe()
+  }
 }
