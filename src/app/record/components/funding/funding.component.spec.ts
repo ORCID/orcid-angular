4d6627a689ae8a2173a1f22973b9542da058fc01
Angel <a.montenegro@orcid.org>
2021-03-15 16:54:00 -0600

    Working on the view

--- src/app/record/components/funding/funding.component.spec.ts
+++ src/app/record/components/funding/funding.component.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { AffiliationComponent } from './affiliation.component'
+import { FundingComponent } from './funding.component'
 
-describe('AffiliationComponent', () => {
-  let component: AffiliationComponent
-  let fixture: ComponentFixture<AffiliationComponent>
+describe('FundingComponent', () => {
+  let component: FundingComponent
+  let fixture: ComponentFixture<FundingComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AffiliationComponent],
+      declarations: [FundingComponent],
     }).compileComponents()
   }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AffiliationComponent)
+    fixture = TestBed.createComponent(FundingComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
   })
