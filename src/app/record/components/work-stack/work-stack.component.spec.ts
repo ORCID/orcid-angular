3666e0e334ceda15cfea40a466f8768baac1fd35
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2021-03-23 08:54:59 -0600

    Final checks

--- src/app/record/components/work-stack/work-stack.component.spec.ts
+++ src/app/record/components/work-stack/work-stack.component.spec.ts
@@ -1,25 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { WorkStackComponent } from './work-stack.component';
+import { WorkStackComponent } from './work-stack.component'
 
 describe('WorkStackComponent', () => {
-  let component: WorkStackComponent;
-  let fixture: ComponentFixture<WorkStackComponent>;
+  let component: WorkStackComponent
+  let fixture: ComponentFixture<WorkStackComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WorkStackComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [WorkStackComponent],
+    }).compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(WorkStackComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(WorkStackComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
