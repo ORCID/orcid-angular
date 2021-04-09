2f73e77f9bce36dbf7a49ddbe94d410c455b7dbb
Angel <a.montenegro@orcid.org>
2021-04-08 18:49:49 -0600

    Display of funding

--- src/app/shared/pipes/contributors-pipe/contributors.pipe.ts
+++ src/app/shared/pipes/contributors-pipe/contributors.pipe.ts
@@ -8,11 +8,25 @@ export class ContributorsPipe implements PipeTransform {
   transform(contributor: Contributor, args?: any): string {
     let value = ''
     if (contributor) {
-      value += contributor.contributorRole ? contributor.contributorRole.value : ''
-      value += contributor.contributorSequence ? this.addComma(value) + contributor.contributorSequence.value : ''
-      value += contributor.orcid ? this.addComma(value) + contributor.orcid.value : ''
-      value += contributor.email ?  this.addComma(value) + contributor.email.value : ''
-      value = value.length > 0 ? '(' + value + ')' : ''
+      if(contributor.contributorRole && contributor.contributorRole.value) {
+        value = contributor.contributorRole.value;
+      }
+      
+      if(contributor.contributorSequence && contributor.contributorSequence.value) {
+        value = this.addComma(value) + contributor.contributorSequence.value
+      }
+      
+      if(contributor.orcid && contributor.orcid.value) {
+        value = this.addComma(value) + contributor.orcid.value
+      } 
+      
+      if(contributor.email && contributor.email.value) {
+        value = this.addComma(value) + contributor.email.value
+      }
+      
+      if(value.length > 0) {
+        value = '(' + value + ')'
+      } 
     }
     return value
   }
