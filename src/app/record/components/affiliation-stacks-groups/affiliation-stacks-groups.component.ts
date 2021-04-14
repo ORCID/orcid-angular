2c99a35c654ce4b52ec06b90650d786b97bcf69f
Daniel Palafox <dp852@hotmail.com>
2021-04-14 15:08:06 -0500

    fix styles in my orcid

--- src/app/record/components/affiliation-stacks-groups/affiliation-stacks-groups.component.ts
+++ src/app/record/components/affiliation-stacks-groups/affiliation-stacks-groups.component.ts
@@ -5,7 +5,7 @@ import { UserService } from 'src/app/core'
 import { RecordService } from 'src/app/core/record/record.service'
 import {
   AffiliationGroup,
-  AffiliationUIGroup,
+  AffiliationUIGroup, AffiliationUIGroupsTypes,
 } from 'src/app/types/record-affiliation.endpoint'
 import { UserRecord } from 'src/app/types/record.local'
 import { UserSession } from 'src/app/types/session.local'
@@ -52,5 +52,24 @@ export class AffiliationStacksGroupsComponent implements OnInit {
 
   trackByAffiliationGroup(index, item: AffiliationGroup) {
     return item.activePutCode
+  }
+
+  affiliationTypeLabel(affiliationType: string) {
+    switch (affiliationType) {
+      case AffiliationUIGroupsTypes.EMPLOYMENT:
+        return $localize`:@@shared.employment:Employment`
+      case AffiliationUIGroupsTypes.EDUCATION_AND_QUALIFICATION:
+        return $localize`:@@shared.educationQualifications:Education and Qualifications`
+      case AffiliationUIGroupsTypes.INVITED_POSITION_AND_DISTINCTION:
+        return $localize`:@@shared.invitedPositions:Invited positions and Distinctions`
+      case AffiliationUIGroupsTypes.MEMBERSHIP_AND_SERVICE:
+        return $localize`:@@shared.membership:Membership and Service`
+    }
+  }
+
+  getAffiliationType(type: string): AffiliationUIGroup {
+    return this.profileAffiliationUiGroups.filter((affiliation) => {
+        return affiliation.type === type
+      })[0]
   }
 }
