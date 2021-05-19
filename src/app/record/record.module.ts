d419b6e26649a58757d0c8365c3de65e6c861ef4
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2021-05-19 12:16:35 -0600

    Revert "Add loading bars"
    This reverts commit c3a1ef9ea29c3d7a4c343224675fa271adc0488e.

--- src/app/record/record.module.ts
+++ src/app/record/record.module.ts
@@ -37,7 +37,6 @@ import { WorkComponent } from './components/work/work.component'
 import { TopBarActionsComponent } from './components/top-bar-actions/top-bar-actions.component'
 import { MatDividerModule } from '@angular/material/divider'
 import { TopBarMyPublicRecidPreviewComponent } from './components/top-bar-my-public-recid-preview/top-bar-my-public-recid-preview.component'
-import { MatProgressBarModule } from '@angular/material/progress-bar'
 
 @NgModule({
   declarations: [
@@ -83,7 +82,6 @@ import { MatProgressBarModule } from '@angular/material/progress-bar'
     MatButtonModule,
     MatDividerModule,
     A11yLinkModule,
-    MatProgressBarModule,
   ],
 })
 export class RecordModule {}
