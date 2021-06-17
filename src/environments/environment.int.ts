92b28d8c1858a5fcee7ab68fb260ef20b7d3474d
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2021-06-17 08:58:38 -0600

    Add support to the int environment

--- src/environments/environment.int.ts
+++ src/environments/environment.int.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   debugger: true,
+  PUBLIC_PAGE_WITH_NO_QA_PREFIX: true,
   API_NEWS_DEPRECATED: 'https://int.orcid.org/blog/feed',
   API_NEWS: 'https://info.int.orcid.org/feed/',
   API_PUB: '//pub.int.orcid.org/v3.0',
