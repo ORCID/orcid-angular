56fce3744b9ab58764c1c664700c206ab88f2648
Daniel Palafox <dp852@hotmail.com>
2021-07-01 18:58:34 -0500

    fix date pipe

--- src/app/shared/pipes/month-day-year-date-to-string/month-day-year-date-to-string.pipe.ts
+++ src/app/shared/pipes/month-day-year-date-to-string/month-day-year-date-to-string.pipe.ts
@@ -8,14 +8,14 @@ export class MonthDayYearDateToStringPipe implements PipeTransform {
   transform(value: MonthDayYearDate, args?: any): any {
     return value
       ? value.year
-        ? value.year.padStart(4, '0') + // Has a year to display
-          (value.month.padStart(2, '0')
-            ? '-' +
-              value.month.padStart(2, '0') + // Has a month to display
-              (value.day.padStart(2, '0')
-                ? '-' + value.day.padStart(2, '0') // Has a day to display
-                : '') // Has NO day to display
-            : '') // Has NO month to display
+        ? value.year?.padStart(4, '0') + // Has a year to display
+        (value.month?.padStart(2, '0')
+          ? '-' +
+          value.month?.padStart(2, '0') + // Has a month to display
+          (value.day?.padStart(2, '0')
+            ? '-' + value.day?.padStart(2, '0') // Has a day to display
+            : '') // Has NO day to display
+          : '') // Has NO month to display
         : '' // Has NO year to display
       : ''
   }
