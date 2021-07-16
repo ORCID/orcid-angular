c0b5d358ce98d2c16f16ef3b8b2520aff13c6691
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2021-07-16 13:59:00 -0600

    add translations

--- src/app/types/works.endpoint.ts
+++ src/app/types/works.endpoint.ts
@@ -71,65 +71,64 @@ export enum WorkOtherOutputTypes {
 }
 
 export const WorkCategoriesLabel = {
-  [WorkCategories.conference]: $localize`Conference`,
-  [WorkCategories.intellectual_property]: $localize`Intellectual property`,
-  [WorkCategories.other_output]: $localize`Other output`,
-  [WorkCategories.publication]: $localize`Publication`,
+  [WorkCategories.conference]: $localize`:@@works.conference:Conference`,
+  [WorkCategories.intellectual_property]: $localize`:@@works.intellectual:Intellectual property`,
+  [WorkCategories.other_output]: $localize`:@@works.other:Other output`,
+  [WorkCategories.publication]: $localize`:@@works.publication:Publication`,
 }
 
 export const WorkTypesLabels = {
   [WorkCategories.conference]: {
-    [WorkConferenceTypes.conferenceAbstract]: 'Conference abstract',
-    [WorkConferenceTypes.conferencePaper]: 'Conference paper',
-    [WorkConferenceTypes.conferencePoster]: 'Conference poster',
+    [WorkConferenceTypes.conferenceAbstract]: $localize`:@@works.abstract:Conference abstract`,
+    [WorkConferenceTypes.conferencePaper]: $localize`:@@works.paper:Conference paper`,
+    [WorkConferenceTypes.conferencePoster]: $localize`:@@works.poster:Conference poster`,
   },
   [WorkCategories.publication]: {
-    [WorkPublicationTypes.bookChapter]: 'Book chapter',
-    [WorkPublicationTypes.bookReview]: 'Book review',
-    [WorkPublicationTypes.book]: 'Book',
-    [WorkPublicationTypes.dictionaryEntry]: 'Dictionary entry',
-    [WorkPublicationTypes.dissertationThesis]: 'Dissertation/Thesis',
-    [WorkPublicationTypes.editedBook]: 'Edited book',
-    [WorkPublicationTypes.encyclopediaEntry]: 'Encyclopedia entry',
-    [WorkPublicationTypes.journalArticle]: 'Journal article',
-    [WorkPublicationTypes.journalIssue]: 'Journal issue',
-    [WorkPublicationTypes.magazineArticle]: 'Magazine article',
-    [WorkPublicationTypes.manual]: 'Manual',
-    [WorkPublicationTypes.newsletterArticle]: 'Newsletter article',
-    [WorkPublicationTypes.newspaperArticle]: 'Newspaper article',
-    [WorkPublicationTypes.onlineResource]: 'Online resource',
-    [WorkPublicationTypes.preprint]: 'Preprint',
-    [WorkPublicationTypes.report]: 'Report',
-    [WorkPublicationTypes.researchTool]: 'Research tool',
-    [WorkPublicationTypes.supervisedStudentPublication]:
-      'Supervised student publication',
-    [WorkPublicationTypes.test]: 'Test',
-    [WorkPublicationTypes.translation]: 'Translation',
-    [WorkPublicationTypes.website]: 'Website',
-    [WorkPublicationTypes.workingPaper]: 'Working paper',
+    [WorkPublicationTypes.bookChapter]: $localize`:@@works.chapter:Book chapter`,
+    [WorkPublicationTypes.bookReview]: $localize`:@@works.review:Book review`,
+    [WorkPublicationTypes.book]: $localize`:@@works.book:Book`,
+    [WorkPublicationTypes.dictionaryEntry]: $localize`:@@works.entry:Dictionary entry`,
+    [WorkPublicationTypes.dissertationThesis]: $localize`:@@works.thesis:Dissertation/Thesis`,
+    [WorkPublicationTypes.editedBook]: $localize`:@@works.editedBook:Edited book`,
+    [WorkPublicationTypes.encyclopediaEntry]: $localize`:@@works.encyclopediaEntry:Encyclopedia entry`,
+    [WorkPublicationTypes.journalArticle]: $localize`:@@works.journalArticle:Journal article`,
+    [WorkPublicationTypes.journalIssue]: $localize`:@@works.issue:Journal issue`,
+    [WorkPublicationTypes.magazineArticle]: $localize`:@@works.magazineArticle:Magazine article`,
+    [WorkPublicationTypes.manual]: $localize`:@@works.manual:Manual`,
+    [WorkPublicationTypes.newsletterArticle]: $localize`:@@works.newsletterarticle:Newsletter article`,
+    [WorkPublicationTypes.newspaperArticle]: $localize`:@@works.newspaperArticle:Newspaper article`,
+    [WorkPublicationTypes.onlineResource]: $localize`:@@works.onlineResource:Online resource`,
+    [WorkPublicationTypes.preprint]: $localize`:@@works.preprint:Preprint`,
+    [WorkPublicationTypes.report]: $localize`:@@works.report:Report`,
+    [WorkPublicationTypes.researchTool]: $localize`:@@works.tool:Research tool`,
+    [WorkPublicationTypes.supervisedStudentPublication]: $localize`:@@works.studentPublication:Supervised student publication`,
+    [WorkPublicationTypes.test]: $localize`:@@works.test:Test`,
+    [WorkPublicationTypes.translation]: $localize`:@@works.translation:Translation`,
+    [WorkPublicationTypes.website]: $localize`:@@works.website:Website`,
+    [WorkPublicationTypes.workingPaper]: $localize`:@@works.working paperAA:Working paper`,
   },
 
   [WorkCategories.intellectual_property]: {
-    [WorkIntellectualPropertyTypes.disclosure]: 'Disclosure',
-    [WorkIntellectualPropertyTypes.license]: 'License',
-    [WorkIntellectualPropertyTypes.patent]: 'Patent',
-    [WorkIntellectualPropertyTypes.registeredCopyright]: 'Registered copyright',
-    [WorkIntellectualPropertyTypes.trademark]: 'Trademark',
+    [WorkIntellectualPropertyTypes.disclosure]: $localize`:@@works.disclosure:Disclosure`,
+    [WorkIntellectualPropertyTypes.license]: $localize`:@@works.license:License`,
+    [WorkIntellectualPropertyTypes.patent]: $localize`:@@works.patent:Patent`,
+    [WorkIntellectualPropertyTypes.registeredCopyright]: $localize`:@@works.copyright:Registered copyright`,
+    [WorkIntellectualPropertyTypes.trademark]: $localize`:@@works.trademark:Trademark`,
   },
   [WorkCategories.other_output]: {
-    [WorkOtherOutputTypes.annotation]: 'Annotation',
-    [WorkOtherOutputTypes.artisticPerformance]: 'Artistic performance',
-    [WorkOtherOutputTypes.dataManagementPlan]: 'Data management plan',
-    [WorkOtherOutputTypes.dataSet]: 'Data set',
-    [WorkOtherOutputTypes.invention]: 'Invention',
-    [WorkOtherOutputTypes.lectureSpeech]: 'Lecture speech',
-    [WorkOtherOutputTypes.physicalObject]: 'Physical object',
-    [WorkOtherOutputTypes.researchTechnique]: 'Research technique',
-    [WorkOtherOutputTypes.software]: 'software',
-    [WorkOtherOutputTypes.spinOffCompany]: 'Spin off company',
-    [WorkOtherOutputTypes.standardsAndPolicy]: 'Standards and policy',
-    [WorkOtherOutputTypes.technicalStandard]: 'Technical Standard',
-    [WorkOtherOutputTypes.other]: 'Other',
+    [WorkOtherOutputTypes.annotation]: $localize`:@@works.annotation:Annotation`,
+    [WorkOtherOutputTypes.artisticPerformance]: $localize`:@@works.artisticPerformance:Artistic performance`,
+    [WorkOtherOutputTypes.dataManagementPlan]: $localize`:@@works.AAA:Data management plan`,
+    [WorkOtherOutputTypes.dataSet]: $localize`:@@works.plan:Data set`,
+    [WorkOtherOutputTypes.invention]: $localize`:@@works.invention:Invention`,
+    [WorkOtherOutputTypes.lectureSpeech]: $localize`:@@works.speech:Lecture speech`,
+    [WorkOtherOutputTypes.physicalObject]: $localize`:@@works.object:Physical object`,
+    [WorkOtherOutputTypes.researchTechnique]: $localize`:@@works.technique:Research technique`,
+    [WorkOtherOutputTypes.software]: $localize`:@@works.software:software`,
+    [WorkOtherOutputTypes.spinOffCompany]: $localize`:@@works.company:Spin off company`,
+    [WorkOtherOutputTypes.standardsAndPolicy]: $localize`:@@works.policy:Standards and policy`,
+    [WorkOtherOutputTypes.technicalStandard]: $localize`:@@works.standard:Technical Standard`,
+    [WorkOtherOutputTypes.other]: $localize`:@@works.other:Other`,
   },
 }
 
@@ -146,21 +145,20 @@ export enum WorksTitleName {
 }
 
 export const WorksTitleNameLabel = {
-  [WorksTitleName.publisher]: 'Publisher',
-  [WorksTitleName.conferenceTitle]: 'Conference title',
-  [WorksTitleName.bookTitle]: 'Book title',
-  [WorksTitleName.journalTitle]: 'Journal title',
-  [WorksTitleName.magazineTitle]: 'Magazine title',
-  [WorksTitleName.newsletterTitle]: 'Newsletter title',
-  [WorksTitleName.institution]: 'Institution',
-  [WorksTitleName.custodian]: 'Custodian',
-  [WorksTitleName.trademark]: 'Trademark',
+  [WorksTitleName.publisher]: $localize`:@@works.AAA:Publisher`,
+  [WorksTitleName.conferenceTitle]: $localize`:@@works.AAA:Conference title`,
+  [WorksTitleName.bookTitle]: $localize`:@@works.AAA:Book title`,
+  [WorksTitleName.journalTitle]: $localize`:@@works.AAA:Journal title`,
+  [WorksTitleName.magazineTitle]: $localize`:@@works.AAA:Magazine title`,
+  [WorksTitleName.newsletterTitle]: $localize`:@@works.AAA:Newsletter title`,
+  [WorksTitleName.institution]: $localize`:@@works.AAA:Institution`,
+  [WorksTitleName.custodian]: $localize`:@@works.AAA:Custodian`,
+  [WorksTitleName.trademark]: $localize`:@@works.AAA:Trademark`,
 }
 
 export const WorkTypesTitle = {
   [WorkCategories.conference]: {
-    [WorkConferenceTypes.conferenceAbstract]:
-      WorksTitleName.conferenceTitle,
+    [WorkConferenceTypes.conferenceAbstract]: WorksTitleName.conferenceTitle,
     [WorkConferenceTypes.conferencePaper]: WorksTitleName.conferenceTitle,
     [WorkConferenceTypes.conferencePoster]: WorksTitleName.conferenceTitle,
   },
@@ -176,10 +174,8 @@ export const WorkTypesTitle = {
     [WorkPublicationTypes.journalIssue]: WorksTitleName.journalTitle,
     [WorkPublicationTypes.magazineArticle]: WorksTitleName.magazineTitle,
     [WorkPublicationTypes.manual]: WorksTitleName.publisher,
-    [WorkPublicationTypes.newsletterArticle]:
-      WorksTitleName.newsletterTitle,
-    [WorkPublicationTypes.newspaperArticle]:
-      WorksTitleName.newsletterTitle,
+    [WorkPublicationTypes.newsletterArticle]: WorksTitleName.newsletterTitle,
+    [WorkPublicationTypes.newspaperArticle]: WorksTitleName.newsletterTitle,
     [WorkPublicationTypes.onlineResource]: WorksTitleName.publisher,
     [WorkPublicationTypes.preprint]: WorksTitleName.journalTitle,
     [WorkPublicationTypes.report]: WorksTitleName.institution,
@@ -616,19 +612,15 @@ export enum WorkRelationships {
 }
 
 export const WorkRelationshipsLabels = {
-  self: 'Self',
-  'part-of': 'Part of',
-  'version-of': 'Version of',
-  'funded-by': 'Funded by',
+  self: $localize`:@@works.self:Self`,
+  'part-of': $localize`:@@works.partOf:Part of`,
+  'version-of': $localize`:@@works.versionOf:Version of`,
+  'funded-by': $localize`:@@works.fundedBy:Funded by`,
 }
 
 export const WorkRelationshipsHintsLabels = {
-  self:
-    'The identifier applies to the work itself. For example, a DOI for a book chapter.',
-  'part-of':
-    'The identifier applies to part of a larger work. For example, the ISBN of the book in which the work was published.',
-  'version-of':
-    'The identifier applies to an alternate version of the work. For example, an earlier draft of an article.',
-  'funded-by':
-    'The identifier applies to an alternate version of the work. For example, an earlier draft of an article.',
+  self: $localize`:@@works.selfDescription:The identifier applies to the work itself. For example, a DOI for a book chapter.`,
+  'part-of': $localize`:@@works.partOfDescription:The identifier applies to part of a larger work. For example, the ISBN of the book in which the work was published.`,
+  'version-of': $localize`:@@works.versionOfDescription:The identifier applies to an alternate version of the work. For example, an earlier draft of an article.`,
+  'funded-by': $localize`:@@works.fundedByDescription:The identifier applies to an alternate version of the work. For example, an earlier draft of an article.`,
 }
