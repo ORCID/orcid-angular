00a393e3b71d8176a83d51bbb26438039fc087e0
Daniel Palafox <dp852@hotmail.com>
2021-07-20 16:04:00 -0500

    fix modal delete

--- src/app/record/components/work/modals/modal-delete-works/modal-delete-works.component.ts
+++ src/app/record/components/work/modals/modal-delete-works/modal-delete-works.component.ts
@@ -6,7 +6,7 @@ import { ModalComponent } from '../../../../../cdk/modal/modal/modal.component'
 import { RecordWorksService } from '../../../../../core/record-works/record-works.service'
 import { PlatformInfoService } from '../../../../../cdk/platform-info'
 import { takeUntil } from 'rxjs/operators'
-import { FormGroup } from '@angular/forms'
+import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-modal-delete-works',
@@ -21,9 +21,10 @@ export class ModalDeleteWorksComponent implements OnInit, OnDestroy {
 
   isMobile: boolean
   loadingWorks = false
+  savingWorks = false
   putCodes: string[] = []
   works: Work[] = []
-  worksChecked: Work[] = []
+  selectedWorks: string[] = []
   selectAll: false
 
   deleteForm: FormGroup
@@ -35,48 +36,76 @@ export class ModalDeleteWorksComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit(): void {
+    const group: { [key: string]: FormGroup } = {}
+
     this._platform
       .get()
       .pipe(takeUntil(this.$destroy))
       .subscribe((platform) => {
         this.isMobile = platform.columns4 || platform.columns8
       })
+
     if (this.putCodes.length > 0) {
       this.loadingWorks = true
       this.putCodes.forEach((putCode, index) => {
         this._recordWorksService
           .getWorkInfo(putCode)
           .subscribe((work: Work) => {
-            this.loadingWorks = false
             this.works.push(work)
-            console.log(index)
-            console.log(JSON.stringify(this.works))
+            if (index === (this.putCodes.length - 1)) {
+              this.works.forEach((w) => {
+                group[w.putCode.value] = new FormGroup({
+                  checked: new FormControl(false)
+                })
+              })
+              this.deleteForm = new FormGroup(group)
+              this.loadingWorks = false
+            }
           })
       })
     }
-    // this.deleteForm = new FormGroup({
-    //   ''
-    // })
   }
 
-  updateCheck() {
-    if (this.selectAll) {
-      // this.workGroup.groups.map((workGroup) => {
-      //   workGroup.works[0].checked = true
-      // })
+  updateCheckAll() {
+    this.putCodes.forEach(value => {
+      if (this.selectedWorks.includes(value)) {
+        if (!!this.selectAll === false) {
+          this.selectedWorks = this.selectedWorks.filter(putCode => putCode !== value)
+        }
+      } else {
+        this.selectedWorks.push(value)
+      }
+      this.deleteForm.patchValue({
+        [value]: {
+          checked: !!this.selectAll
+        }
+      })
+    })
+  }
+
+  updateCheck(putCode: string) {
+    if (this.selectedWorks.includes(putCode)) {
+      if (!!this.selectAll === false) {
+        this.selectedWorks = this.selectedWorks.filter(value => value !== putCode)
+      }
     } else {
-      // this.workGroup.groups.map((workGroup) => {
-      //   workGroup.works[0].checked = false
-      // })
+      this.selectedWorks.push(putCode)
     }
   }
 
   saveEvent() {
-    this.loadingWorks = true
+    const putCodesDelete = []
+    this.savingWorks = true
+    this.works
+      .forEach((work, i) => {
+        if (this.deleteForm.value[work.putCode.value].checked) {
+          putCodesDelete.push(work.putCode.value)
+        }
+      })
     this._recordWorksService
-      .delete(this.putCodes)
+      .delete(putCodesDelete)
       .subscribe(() => {
-        this.loadingWorks = false
+        this.savingWorks = false
         this.closeEvent()
         }
       )
