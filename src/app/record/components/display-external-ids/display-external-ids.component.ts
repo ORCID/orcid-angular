8c8251f1a921150b8a4539c3f3ad01bd168c4e97
Daniel Palafox <dp852@hotmail.com>
2021-07-30 16:16:22 -0500

    update component to use single external identifier and rm comma and add space in Convening organization

--- src/app/record/components/display-external-ids/display-external-ids.component.ts
+++ src/app/record/components/display-external-ids/display-external-ids.component.ts
@@ -9,8 +9,14 @@ import { ExternalIdentifier } from 'src/app/types/common.endpoint'
 export class DisplayExternalIdsComponent implements OnInit {
   @Input() externalIdentifiers: ExternalIdentifier[]
   @Input() noBoldTitle = false
+  @Input() externalIdentifier: ExternalIdentifier
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.externalIdentifier) {
+      this.externalIdentifiers = []
+      this.externalIdentifiers.push(this.externalIdentifier)
+    }
+  }
 }
