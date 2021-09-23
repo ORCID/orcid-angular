8a72966d7b030e8aade3602b730ac388d9b6c485
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2021-09-23 12:13:13 -0600

    Add other names sources

--- src/app/cdk/panel/panel-element-source/panel-element-source.component.ts
+++ src/app/cdk/panel/panel-element-source/panel-element-source.component.ts
@@ -9,6 +9,7 @@ import { Component, Input, OnInit } from '@angular/core'
   ],
 })
 export class PanelElementSourceComponent implements OnInit {
+  @Input() hideSourceDivider: boolean
   @Input() name: string
   @Input() date: string
   @Input() isLastItem: boolean
