83a4655eb3a8bcc1d0daacedea1b5cb503f070c5
Camelia <c.dumitru@orcid.org>
2021-09-30 13:14:30 +0100

    Added the validation for translated title

---
+++ src/app/shared/validators/translated-title/translated-title.validator.ts
@@ -0,0 +1,15 @@
+import { AbstractControl, FormGroup } from '@angular/forms'
+
+export function translatedTitleValidator({ value }: FormGroup): { [key: string]: any } {
+    const translatedTitleContent = value?.translatedTitleContent
+    const translatedTitleLanguage = value?.translatedTitleLanguage
+    if (translatedTitleContent && !translatedTitleLanguage ) {
+        return { invalidTranslatedTitleLanguage: true }      
+    }
+    else if (!translatedTitleContent && translatedTitleLanguage) {
+        return { invalidTranslatedTitleContent: true }
+    }
+    else {
+        return null
+    }
+}
