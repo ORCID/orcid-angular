edcf1baa0c7028003e3ded531d2db820b317ca0e
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2021-10-20 11:25:59 -0600

    Add visibility control to peer reviews

--- src/app/cdk/panel/panel/panel.component.ts
+++ src/app/cdk/panel/panel/panel.component.ts
@@ -20,7 +20,10 @@ import { PlatformInfoService } from '../../platform-info'
 import { first } from 'rxjs/operators'
 import { Affiliation } from 'src/app/types/record-affiliation.endpoint'
 import { Funding } from 'src/app/types/record-funding.endpoint'
-import { PeerReview } from '../../../types/record-peer-review.endpoint'
+import {
+  PeerReview,
+  PeerReviewDuplicateGroup,
+} from '../../../types/record-peer-review.endpoint'
 import { Work } from 'src/app/types/record-works.endpoint'
 import { FormGroup } from '@angular/forms'
 import { RecordAffiliationService } from '../../../core/record-affiliations/record-affiliations.service'
@@ -39,9 +42,8 @@ import { WINDOW } from 'src/app/cdk/window'
   styleUrls: ['./panel.component.scss', 'panel.component.scss-theme.scss'],
 })
 export class PanelComponent implements OnInit {
-  @Input() isPreferred = false
+  @Input() showVisibilityControl = false
   @Input() stackSiblings: any[]
-
   @Input() stackedHeader = false
   @Input() expandButtonsPosition: 'right' | 'left' = null
   @Input() editModalComponent: ComponentType<any>
@@ -234,6 +236,11 @@ export class PanelComponent implements OnInit {
   }
 
   updateVisibility(visibility: VisibilityStrings) {
+    console.log(visibility)
+    console.log(this.type)
+
+    console.log(this.stackSiblings)
+
     switch (this.type) {
       case 'employment':
       case 'education':
@@ -289,7 +296,7 @@ export class PanelComponent implements OnInit {
         this._peerReviewService
           .updateVisibility(
             this.stackSiblings.reduce(
-              (p, c) => p + (c.putCode as Value).value + `,`,
+              (p, c) => p + (c as PeerReviewDuplicateGroup).id + `,`,
               ''
             ),
             visibility
