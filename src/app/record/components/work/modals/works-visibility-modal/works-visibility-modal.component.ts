928aeab34038fd9a490dbf3ebf31c5d754f487f4
Daniel Palafox <dp852@hotmail.com>
2021-10-27 19:14:40 -0500

    fix: Add obo to combine, export and visibility modals

--- src/app/record/components/work/modals/works-visibility-modal/works-visibility-modal.component.ts
+++ src/app/record/components/work/modals/works-visibility-modal/works-visibility-modal.component.ts
@@ -5,6 +5,8 @@ import { MatDialogRef } from '@angular/material/dialog'
 import { ModalComponent } from '../../../../../cdk/modal/modal/modal.component'
 import { RecordWorksService } from '../../../../../core/record-works/record-works.service'
 import { FormControl, FormGroup } from '@angular/forms'
+import { PlatformInfoService } from '../../../../../cdk/platform-info'
+import { takeUntil } from 'rxjs/operators'
 
 @Component({
   selector: 'app-works-visibility-modal',
@@ -15,16 +17,25 @@ export class WorksVisibilityModalComponent implements OnInit, OnDestroy {
   $destroy: Subject<boolean> = new Subject<boolean>()
 
   worksForm: FormGroup
+  isMobile: boolean
   loadingWorks = false
   putCodes: string[] = []
   works: Work[] = []
 
   constructor(
     public dialogRef: MatDialogRef<ModalComponent>,
+    private _platform: PlatformInfoService,
     private _recordWorksService: RecordWorksService
   ) {}
 
   ngOnInit(): void {
+    this._platform
+      .get()
+      .pipe(takeUntil(this.$destroy))
+      .subscribe((platform) => {
+        this.isMobile = platform.columns4 || platform.columns8
+      })
+
     this.worksForm = new FormGroup({
       visibility: new FormControl('PRIVATE', {}),
     })
