80c5971bd0e52792f786c8f55890adb7b705d5b0
Camelia <c.dumitru@orcid.org>
2021-11-02 13:02:00 +0000

    Fixed the default visibility for change bulk works

--- src/app/record/components/work/modals/works-visibility-modal/works-visibility-modal.component.ts
+++ src/app/record/components/work/modals/works-visibility-modal/works-visibility-modal.component.ts
@@ -4,9 +4,12 @@ import { Work } from '../../../../../types/record-works.endpoint'
 import { MatDialogRef } from '@angular/material/dialog'
 import { ModalComponent } from '../../../../../cdk/modal/modal/modal.component'
 import { RecordWorksService } from '../../../../../core/record-works/record-works.service'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { PlatformInfoService } from '../../../../../cdk/platform-info'
 import { takeUntil } from 'rxjs/operators'
+import { VisibilityStrings } from 'src/app/types/common.endpoint'
+import { UserRecord } from 'src/app/types/record.local'
+import { RecordService } from 'src/app/core/record/record.service'
 
 @Component({
   selector: 'app-works-visibility-modal',
@@ -17,15 +20,18 @@ export class WorksVisibilityModalComponent implements OnInit, OnDestroy {
   $destroy: Subject<boolean> = new Subject<boolean>()
 
   worksForm: FormGroup
+  defaultVisibility: VisibilityStrings
   isMobile: boolean
   loadingWorks = false
   putCodes: string[] = []
   works: Work[] = []
+  ngOrcidDefaultVisibilityLabel = $localize`:@@shared.whoCanSeeWorks:Who can see your works. Your default visibility setting is`
 
   constructor(
     public dialogRef: MatDialogRef<ModalComponent>,
     private _platform: PlatformInfoService,
-    private _recordWorksService: RecordWorksService
+    private _recordWorksService: RecordWorksService,
+    private _record: RecordService
   ) {}
 
   ngOnInit(): void {
@@ -35,10 +41,23 @@ export class WorksVisibilityModalComponent implements OnInit, OnDestroy {
       .subscribe((platform) => {
         this.isMobile = platform.columns4 || platform.columns8
       })
+   
 
     this.worksForm = new FormGroup({
-      visibility: new FormControl('PRIVATE', {}),
+      visibility: new FormControl(this.defaultVisibility, {
+        validators: [Validators.required],
+      }),
     })
+
+    //
+    this._record
+      .getPreferences()
+      .subscribe((userRecord) => {
+        this.defaultVisibility = userRecord.default_visibility
+        this.worksForm.patchValue({
+          visibility: this.defaultVisibility,
+        })
+      })
 
     if (this.putCodes.length > 0) {
       this.loadingWorks = true
