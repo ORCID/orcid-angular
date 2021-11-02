9dbfd9f3312251dc4fe802842189083d0111e46b
Camelia <c.dumitru@orcid.org>
2021-11-02 11:44:50 +0000

    fixed the property name in localize for whoCanSeeBio

--- src/app/record/components/top-bar/modals/modal-biography/modal-biography.component.ts
+++ src/app/record/components/top-bar/modals/modal-biography/modal-biography.component.ts
@@ -44,7 +44,7 @@ export class ModalBiographyComponent implements OnInit, OnDestroy {
   platform: PlatformInfo
 
   ngOrcidAddYourBiography = $localize`:@@topBar.addYourBiography:Add you biography`
-  ngOrcidDefaultVisibilityLabel=  $localize`:@@shared.whoCanSeeBio.addYourBiography:Who can see your biography. Your default visibility setting is`
+  ngOrcidDefaultVisibilityLabel=  $localize`:@@shared.whoCanSeeBio:Who can see your biography. Your default visibility setting is`
 
   constructor(
     @Inject(WINDOW) private window: Window,
