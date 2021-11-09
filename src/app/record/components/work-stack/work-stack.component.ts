e886f9c994384a57af72f8f4c1fee8a2cbbf97ec
Daniel Palafox <dp852@hotmail.com>
2021-11-09 16:03:34 -0500

    fix: Leo comments

--- src/app/record/components/work-stack/work-stack.component.ts
+++ src/app/record/components/work-stack/work-stack.component.ts
@@ -105,8 +105,8 @@ export class WorkStackComponent implements OnInit {
     // todo @DanielPalafox remove if and togglz once contributors are returned in works page endpoint
     if (
       this.togglzWorksContributors ||
-      this.panelDetailsState[work.putCode.value] !== undefined ||
-      !force
+      (this.panelDetailsState[work.putCode.value] !== undefined &&
+      !force)
     ) {
       this.getDetails(work).subscribe()
     }
