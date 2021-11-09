a48e4f01d96953b09100d58c32f3575949cfd092
Daniel Palafox <dp852@hotmail.com>
2021-11-09 13:06:33 -0500

    fix: Add todo comment

--- src/app/record/components/work-stack/work-stack.component.ts
+++ src/app/record/components/work-stack/work-stack.component.ts
@@ -102,6 +102,7 @@ export class WorkStackComponent implements OnInit {
         state: false,
       }
     }
+    // todo @DanielPalafox remove if and togglz once contributors are returned in works page endpoint
     if (
       this.togglzWorksContributors ||
       this.panelDetailsState[work.putCode.value] !== undefined ||
