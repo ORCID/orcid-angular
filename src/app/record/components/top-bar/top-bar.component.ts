5584521a90ed0d9f3e046f2a2c32975e9796be38
Daniel Palafox <dp852@hotmail.com>
2021-12-09 13:59:57 -0500

    Revert "Moved the display modal logic inside subscribe ( not pipe)"
    This reverts commit 65339831ddc3d2302ffd3238741d67c843cfd64b.

--- src/app/record/components/top-bar/top-bar.component.ts
+++ src/app/record/components/top-bar/top-bar.component.ts
@@ -80,6 +80,27 @@ export class TopBarComponent implements OnInit, OnDestroy {
       .getRecord({
         publicRecordId: this.isPublicRecord || undefined,
       })
+      .pipe(
+        tap((record) => {
+          if (record?.userInfo && !this.isPublicRecord) {
+            const checkEmailValidated =
+              record.userInfo?.IS_PRIMARY_EMAIL_VERIFIED === 'true'
+            const inDelegationMode =
+              record.userInfo.IN_DELEGATION_MODE === 'true'
+            if (!checkEmailValidated && !inDelegationMode) {
+              if (record.emails) {
+                const primaryEmail = record.emails.emails?.filter(
+                  (email) => email.primary
+                )[0]
+                if (!primaryEmail?.verified) {
+                  this.resendVerificationEmailModal(primaryEmail.value)
+                }
+              }
+            }
+          }
+        }),
+        takeUntil(this.$destroy)
+      )
       .subscribe((userRecord) => {
         this.recordWithIssues = userRecord?.userInfo?.RECORD_WITH_ISSUES
         this.userRecord = userRecord
@@ -87,23 +108,6 @@ export class TopBarComponent implements OnInit, OnDestroy {
 
         if (!isEmpty(userRecord.otherNames)) {
           this.setNames(userRecord)
-        }
-
-        if (userRecord?.userInfo && !this.isPublicRecord) {
-          const checkEmailValidated =
-            userRecord.userInfo?.IS_PRIMARY_EMAIL_VERIFIED === 'true'
-          const inDelegationMode =
-            userRecord.userInfo.IN_DELEGATION_MODE === 'true'
-          if (!checkEmailValidated && !inDelegationMode) {
-            if (userRecord.emails) {
-              const primaryEmail = userRecord.emails.emails.filter(
-                (email) => email.primary
-              )[0]
-              if (!primaryEmail?.verified) {
-                this.resendVerificationEmailModal(primaryEmail.value)
-              }
-            }
-          }
         }
       })
   }
