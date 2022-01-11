532e518d5cf97e0b564da4a0a880f139115367a3
Daniel Palafox <dp852@hotmail.com>
2022-01-11 15:59:38 -0500

    fix: Increase citation max length, rollback duping contributors

--- src/app/record/components/work/work.component.ts
+++ src/app/record/components/work/work.component.ts
@@ -32,7 +32,7 @@ export class WorkComponent implements OnInit {
   @Input() togglzWorksContributors: boolean
   maxNumberContributors = 10
   maxNumberContributorsWorkDetails = 50
-  maxBibtexCharacters = 1000
+  maxBibtexCharacters = 5000
   contributors: string[] = []
   numberOfContributors: number
   contributorsDetails: Contributor[] = []
@@ -88,7 +88,9 @@ export class WorkComponent implements OnInit {
   getContributors(contributor: Contributor[]) {
     contributor.forEach((c) => {
       if (c?.creditName?.value) {
-        this.contributors.push(c?.creditName?.value)
+        if (!this.contributors.includes(c?.creditName?.value)) {
+          this.contributors.push(c?.creditName?.value)
+        }
       }
     })
     this.numberOfContributors = this.contributors.length
@@ -102,7 +104,11 @@ export class WorkComponent implements OnInit {
         }
       } else {
         if (c?.orcid?.value === this.id) {
-          this.contributionRole = c?.contributorRole?.value
+          if (this.contributionRole) {
+            this.contributionRole = this.contributionRole + ', ' + c?.contributorRole?.value
+          } else {
+            this.contributionRole = c?.contributorRole?.value
+          }
         }
       }
     })
