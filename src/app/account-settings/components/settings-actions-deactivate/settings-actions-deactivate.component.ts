b857986df8f3c4d9d0c0516c70196689b34b7f40
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2022-02-10 15:27:55 -0600

    Add urls

--- src/app/account-settings/components/settings-actions-deactivate/settings-actions-deactivate.component.ts
+++ src/app/account-settings/components/settings-actions-deactivate/settings-actions-deactivate.component.ts
@@ -9,6 +9,8 @@ import { Subject } from 'rxjs'
 import { takeUntil } from 'rxjs/operators'
 import { PlatformInfoService } from 'src/app/cdk/platform-info'
 import { AccountActionsDeactivateService } from 'src/app/core/account-actions-deactivate/account-actions-deactivate.service'
+import { TogglzService } from 'src/app/core/togglz/togglz.service'
+import { environment } from 'src/environments/environment.production'
 
 @Component({
   selector: 'app-settings-actions-deactivate',
@@ -17,20 +19,31 @@ import { AccountActionsDeactivateService } from 'src/app/core/account-actions-de
     './settings-actions-deactivate.component.scss',
     './settings-actions-deactivate.component.scss-theme.scss',
   ],
+  preserveWhitespaces: true,
 })
 export class SettingsActionsDeactivateComponent implements OnInit, OnDestroy {
   @Output() loading = new EventEmitter<boolean>()
   isMobile: any
   $destroy = new Subject<void>()
   deactivatedEmail
-
+  togglzOrcidAngularAccountSettings: boolean
+  removeDuplicateUrl: string
   constructor(
     private _platform: PlatformInfoService,
-    private _deactivate: AccountActionsDeactivateService
+    private _deactivate: AccountActionsDeactivateService,
+    private _togglz: TogglzService
   ) {}
 
   ngOnInit(): void {
     this.deactivatedEmail = ''
+    this._togglz
+      .getStateOf('ORCID_ANGULAR_ACCOUNT_SETTINGS')
+      .subscribe((value) => {
+        this.togglzOrcidAngularAccountSettings = value
+        this.removeDuplicateUrl = this.togglzOrcidAngularAccountSettings
+          ? '/qa/account'
+          : '/account'
+      })
     this._platform
       .get()
       .pipe(takeUntil(this.$destroy))
