1cd2aa3430d632dbeff91dd28c8ca07608b6fd21
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2022-03-15 19:34:15 -0600

    Show private name for current delegators

--- src/app/core/trusted-individuals/trusted-individuals.service.ts
+++ src/app/core/trusted-individuals/trusted-individuals.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { TrustedIndividuals } from 'src/app/types/trusted-individuals.endpoint'
 import { environment } from 'src/environments/environment'
+import { P } from '@angular/cdk/keycodes'
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root',
@@ -11,11 +13,23 @@ export class TrustedIndividualsService {
   constructor(private _http: HttpClient) {}
 
   getTrustedIndividuals(): Observable<TrustedIndividuals> {
-    return this._http.get<TrustedIndividuals>(
-      `${environment.API_WEB}delegators/delegators-and-me.json`,
-      {
-        withCredentials: true,
-      }
-    )
+    return this._http
+      .get<TrustedIndividuals>(
+        `${environment.API_WEB}delegators/delegators-and-me.json`,
+        {
+          withCredentials: true,
+        }
+      )
+      .pipe(
+        map((x) => {
+          x.delegators.forEach(
+            (x) =>
+              (x.receiverName =
+                x.receiverName ||
+                $localize`:@@account.nameIsPri:Name is private`)
+          )
+          return x
+        })
+      )
   }
 }
