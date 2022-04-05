2c6d3d786decabb4a746f751df961b6f1dcee4f6
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2022-04-04 19:08:54 -0600

    Add funding organization search lines wrapper

--- src/app/core/record-fundings/record-fundings.service.ts
+++ src/app/core/record-fundings/record-fundings.service.ts
@@ -168,20 +168,154 @@ export class RecordFundingsService {
   }
 
   getOrganization(org: string): Observable<Organization[]> {
-    return this._http
-      .get<Organization[]>(
-        environment.API_WEB +
-          'fundings/disambiguated/name/' +
-          org +
-          '?limit=100&funders-only=true',
-        {
-          headers: this.headers,
-        }
-      )
-      .pipe(
-        retry(3),
-        catchError((error) => this._errorHandler.handleError(error))
-      )
+    return of([
+      {
+        sourceId: 'https://ror.org/01f80g185',
+        country: 'CH',
+        orgType: 'Government',
+        countryForDisplay:
+          'org.orcid.persistence.jpa.entities.CountryIsoEntity.CH',
+        disambiguatedAffiliationIdentifier: '2132537',
+        city: 'Geneva',
+        sourceType: 'ROR',
+        region: null,
+        value: 'World Health Organization',
+        url: null,
+        affiliationKey: 'World Health Organization Geneva  CH',
+      },
+      {
+        sourceId: 'https://ror.org/038ejm451',
+        country: 'US',
+        orgType: 'Nonprofit',
+        countryForDisplay:
+          'org.orcid.persistence.jpa.entities.CountryIsoEntity.US',
+        disambiguatedAffiliationIdentifier: '2191119',
+        city: 'Woodbridge',
+        sourceType: 'ROR',
+        region: 'Virginia',
+        value: 'World Federation for Mental Health',
+        url: null,
+        affiliationKey:
+          'World Federation for Mental Health Woodbridge Virginia US',
+      },
+      {
+        sourceId: 'http://dx.doi.org/10.13039/100010829',
+        country: 'FI',
+        orgType: 'pri/Associations and societies (private and public)',
+        countryForDisplay:
+          'org.orcid.persistence.jpa.entities.CountryIsoEntity.FI',
+        disambiguatedAffiliationIdentifier: '434969',
+        city: null,
+        sourceType: 'FUNDREF',
+        region: null,
+        value: 'World Association for Infant Mental Health',
+        url: null,
+        affiliationKey: 'World Association for Infant Mental Health   FI',
+      },
+      {
+        sourceId: 'http://dx.doi.org/10.13039/100019461',
+        country: 'US',
+        orgType: 'gov/Research institutes and centers',
+        countryForDisplay:
+          'org.orcid.persistence.jpa.entities.CountryIsoEntity.US',
+        disambiguatedAffiliationIdentifier: '2251765',
+        city: 'OR',
+        sourceType: 'FUNDREF',
+        region: 'OR',
+        value:
+          'Oregon Institute of Occupational Health Sciences, Oregon Health and Science University',
+        url: null,
+        affiliationKey:
+          'Oregon Institute of Occupational Health Sciences, Oregon Health and Science University OR OR US',
+      },
+      {
+        sourceId: 'https://ror.org/01xmtfq82',
+        country: 'US',
+        orgType: 'Nonprofit',
+        countryForDisplay:
+          'org.orcid.persistence.jpa.entities.CountryIsoEntity.US',
+        disambiguatedAffiliationIdentifier: '2176738',
+        city: 'Portland',
+        sourceType: 'ROR',
+        region: 'Oregon',
+        value: 'Northwest Health Foundation',
+        url: null,
+        affiliationKey: 'Northwest Health Foundation Portland Oregon US',
+      },
+      {
+        sourceId: 'https://ror.org/009avj582',
+        country: 'US',
+        orgType: 'Education',
+        countryForDisplay:
+          'org.orcid.persistence.jpa.entities.CountryIsoEntity.US',
+        disambiguatedAffiliationIdentifier: '2132660',
+        city: 'Portland',
+        sourceType: 'ROR',
+        region: 'Oregon',
+        value: 'Oregon Health & Science University',
+        url: null,
+        affiliationKey: 'Oregon Health & Science University Portland Oregon US',
+      },
+      {
+        sourceId: 'http://dx.doi.org/10.13039/100013363',
+        country: 'US',
+        orgType: 'pri/Other non-profit organizations',
+        countryForDisplay:
+          'org.orcid.persistence.jpa.entities.CountryIsoEntity.US',
+        disambiguatedAffiliationIdentifier: '1715498',
+        city: 'CA',
+        sourceType: 'FUNDREF',
+        region: 'CA',
+        value: 'Sutter Health',
+        url: null,
+        affiliationKey: 'Sutter Health CA CA US',
+      },
+      {
+        sourceId: 'http://dx.doi.org/10.13039/100016634',
+        country: 'US',
+        orgType: 'pri/Other non-profit organizations',
+        countryForDisplay:
+          'org.orcid.persistence.jpa.entities.CountryIsoEntity.US',
+        disambiguatedAffiliationIdentifier: '2108204',
+        city: 'CO',
+        sourceType: 'FUNDREF',
+        region: 'CO',
+        value: 'Vail Health',
+        url: null,
+        affiliationKey: 'Vail Health CO CO US',
+      },
+      {
+        sourceId: 'http://dx.doi.org/10.13039/100016652',
+        country: 'US',
+        orgType: 'pri/Other non-profit organizations',
+        countryForDisplay:
+          'org.orcid.persistence.jpa.entities.CountryIsoEntity.US',
+        disambiguatedAffiliationIdentifier: '2108222',
+        city: 'NC',
+        sourceType: 'FUNDREF',
+        region: 'NC',
+        value: 'Duke Health',
+        url: null,
+        affiliationKey: 'Duke Health NC NC US',
+      },
+      {
+        sourceId: 'http://dx.doi.org/10.13039/100015201',
+        country: 'US',
+        orgType: 'pri/Other non-profit organizations',
+        countryForDisplay:
+          'org.orcid.persistence.jpa.entities.CountryIsoEntity.US',
+        disambiguatedAffiliationIdentifier: '2094982',
+        city: 'NY',
+        sourceType: 'FUNDREF',
+        region: 'NY',
+        value: 'Oscar Health',
+        url: null,
+        affiliationKey: 'Oscar Health NY NY US',
+      },
+    ]).pipe(
+      retry(3),
+      catchError((error) => this._errorHandler.handleError(error))
+    )
   }
 
   loadFundingImportWizardList(): Observable<RecordImportWizard[]> {
