af406375640d0559bf539f316138be0a3332d8be
Karen Madrigal <kprismm@gmail.com>
2022-04-05 21:40:01 -0600

    WIP

--- cypress/integration/registry/reset-password-recover-id.e2e.js
+++ cypress/integration/registry/reset-password-recover-id.e2e.js
@@ -7,48 +7,43 @@ describe('Password reset and OID recovery', () => {
     cy.visit('/')
   })
 
-  it.skip('User resets password', function () {
+  it('User resets password', function () {
     const newPassword = '12345678Ab'
 
     //click Sign in
     cy.get('#menu-signin-button').click()
     //click forgot password link
     cy.get('#forgot-password-button').click()
-    //select password recovery option
-    cy.get('[value="resetPassword"]').should('be.selected').click()
-    //type email
+    //password option is selected by default, type email
     cy.get('[formcontrolname="email"]')
       .clear()
       .type(userData.cyUserPrimaryEmaiVerified.email)
     //click button to recover details
     cy.get('button').contains('RECOVER ACCOUNT DETAILS').click()
-
+  
     //use gmail api to check recovery email was sent
-    cy.task('readAllMessages', {
+    cy.task('checkInbox_from_to_subject', {
       options: {
         from: Cypress.env('senderResetPassword'),
         to: userData.cyUserPrimaryEmaiVerified.email,
         subject: Cypress.env('forgotPasswordSubject'),
         include_body: true,
       },
-    }).then((emails) => {
+    }).then((email) => {
       //there may be multiple emails with same subject and sender
-      assert.isNotNull(emails.length)
+      assert.isNotNull(email)
       //grab most recent email
-      const emailBody = emails[0].body.html
-      //cy.log(">>>>>>>>>Email body is: "+ JSON.stringify(emails[0].body))
+      const emailBody = email.body.html
       //convert string to DOM
       const htmlDom = new DOMParser().parseFromString(emailBody, 'text/html')
       cy.log(htmlDom.documentElement.innerHTML)
 
       //find the link that starts pointing to the correct endpoint
-      const href = htmlDom.querySelector(
-        '[href^=Cypress.env("resetPasswEmailURL")]'
-      ).href
-      cy.log('>>>>>>>found the link: ' + href)
+      const href = htmlDom.querySelector(`a[id*='cy-pwd-reset-url']`).href
+      //follow the link from the email
+      cy.visit(href)
     })
-    //follow the link from the email
-    cy.visit(href)
+
     //type new passw
     cy.get('#passwordField').clear().type(newPassword)
     //confirm new passw
@@ -58,26 +53,29 @@ describe('Password reset and OID recovery', () => {
       cy.get('button').click()
     })
     //verify user is redirected to Sign in page
-    cy.url().should('contain', Cypress.env(signInURL))
+    cy.url().should('contain', Cypress.env('signInURL'))
+    cy.wait(2000)//intentional wait for page to load
 
     //Verify user can sign in with new passw
-    cy.get('username').clear().type(userData.cyUserPrimaryEmailNotVerified.oid)
-    cy.get('password').clear().type(newPassword)
+    cy.get('#username').clear().type(userData.cyUserPrimaryEmaiVerified.oid)
+    cy.get('#password').clear().type(newPassword)
     cy.get('#signin-button').click()
+    cy.wait(2000)//intentional wait for page to load
 
-    /* CLEAN UP FOR FUTURE RUNS */
-    //change passw to original value
-    cy.get('app-user-menu').click()
-    cy.get('#cdk-overlay-0').within(($menu) => {
-      cy.get('.mat-menu-item').contains('Account settings').click()
-    })
-    //edit passw and save
+    //change passw back to original value
+    cy.get('#cy-user-info').click()
+    cy.wait(1000)//intentional wait for page to load
+    cy.get('#cy-account-settings').click()
+    cy.wait(1000)//intentional wait for page to load
+    cy.contains(' Account password ').click()//TO DO: replace with element id once it is added
+    cy.get('[formcontrolname="oldPassword"]').clear().type(newPassword)
+    cy.get('[formcontrolname="password"]').clear().type(userData.cyUserPrimaryEmaiVerified.password)
+    cy.get('[formcontrolname="retypedPassword"]').clear().type(userData.cyUserPrimaryEmaiVerified.password)
+    cy.get('#save-password').click()
 
     //sign out
-    cy.get('app-user-menu').click()
-    cy.get('#cdk-overlay-0').within(($menu) => {
-      cy.get('.mat-menu-item').contains('Logout').click()
-    })
+    cy.get('#cy-user-info').click()
+    cy.get('#cy-signout').click()
   })
 
   it('User recovers OID', function () {
@@ -108,9 +106,7 @@ describe('Password reset and OID recovery', () => {
       //convert string to DOM
       const htmlDom = new DOMParser().parseFromString(emailBody, 'text/html')
       //find the link that contains user OID
-      const href = htmlDom.querySelector(
-        `a[href*='${userData.cyUserPrimaryEmaiVerified.oid}']`
-      ).href
+      const href = htmlDom.querySelector(`a[href*='${userData.cyUserPrimaryEmaiVerified.oid}']`).href
       cy.log('>>>>>>>found the link: ' + href)
       //follow the link from the email
       cy.visit(href)
