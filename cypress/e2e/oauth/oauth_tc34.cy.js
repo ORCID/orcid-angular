87221936006672dc635c6dbfa8abf0f965c949b1
Karen Madrigal <kprismm@gmail.com>
2022-08-29 14:38:05 -0600

    update to validate both empty and missing client id param

--- cypress/e2e/oauth/oauth_tc34.cy.js
+++ cypress/e2e/oauth/oauth_tc34.cy.js
@@ -12,20 +12,31 @@ expected: user is taken to OAuth error page "oauth_error / Please specify a clie
 describe('OAuth cypress tests', async function () {
   const recordOwner = userData.cyOAuth_RecordOwnerTC_Sessions
   const scope = '/person/update'
-  const authorizationLink =
+  const authorizationLinkNoClient =
+    'https://qa.orcid.org/oauth/authorize?' +
+    '&response_type=code&scope=' +
+    scope +
+    '&redirect_uri=' +
+    userData.cyOAuth_MemberUser.redirect_uri
+  
+    const authorizationLinkEmptyClient =
     'https://qa.orcid.org/oauth/authorize?client_id=' +
     '&response_type=code&scope=' +
     scope +
     '&redirect_uri=' +
     userData.cyOAuth_MemberUser.redirect_uri
-
   before(() => {})
 
-  it('TC#34 missing client id in authorization link results in error message', function () {
-    cy.visit(authorizationLink)
+  it('TC#34 empty client id in authorization link results in error message', function () {
+    cy.visit(authorizationLinkEmptyClient)
     cy.wait(2000)
-    cy.get('.oauth-error').contains('Client not found')
+    cy.get('.oauth-error').contains('The provided client id is invalid')
   })
 
+  it('TC#34 missing client id in authorization link results in error message', function () {
+    cy.visit(authorizationLinkNoClient)
+    cy.wait(2000)
+    cy.get('.oauth-error').contains('Incorrect OAuth Link, missing client id parameter')
+  })
   after(() => {})
 })
