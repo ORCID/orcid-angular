eb32bd5d7073b2346f8884708dcf35ba34c061d5
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2022-12-12 10:18:56 -0600

    add missing side-bar aria labels
    (cherry picked from commit 0c806f22769a060d968d948fe9b19f89abafec54)

--- src/app/shared/pipes/app-panels-collapse-aria-label/app-panels-collapse-aria-label.pipe.ts
+++ src/app/shared/pipes/app-panels-collapse-aria-label/app-panels-collapse-aria-label.pipe.ts
@@ -5,25 +5,41 @@ import { Pipe, PipeTransform } from '@angular/core'
 })
 export class AppPanelsCollapseAriaLabelPipe implements PipeTransform {
   transform(panelType: any, args?: any): string {
-    if (panelType == 'employment') {
+    if (panelType === 'employment') {
       return $localize`:@@shared.employmentCollapseAriaLabel:Collapse the Employment section`
-    } else if (panelType == 'education' || panelType == 'qualification') {
+    } else if (panelType === 'education' || panelType === 'qualification') {
       return $localize`:@@shared.educationQualificationCollapseAriaLabel:Collapse the Education and qualifications section`
-    } else if (panelType == 'distinction' || panelType == 'invited-position') {
+    } else if (
+      panelType === 'distinction' ||
+      panelType === 'invited-position'
+    ) {
       return $localize`:@@shared.distinctionInvitedPositionCollapseAriaLabel:Collapse the Invited positions and distinction section`
-    } else if (panelType == 'membership' || panelType == 'service') {
+    } else if (panelType === 'membership' || panelType === 'service') {
       return $localize`:@@shared.membershipServiceCollapseAriaLabel:Collapse the Membership and service section`
-    } else if (panelType == 'funding') {
+    } else if (panelType === 'funding') {
       return $localize`:@@shared.fundingServiceCollapseAriaLabel:Collapse the Funding section`
-    } else if (panelType == 'works') {
+    } else if (panelType === 'works') {
       return $localize`:@@shared.workServiceCollapseAriaLabel:Collapse the Works section`
-    } else if (panelType == 'peer-review') {
+    } else if (panelType === 'peer-review') {
       console.log('in peer-review')
       return $localize`:@@shared.peerReviewServiceCollapseAriaLabel:Collapse the Peer review section`
-    } else if (panelType == 'sub-peer-review') {
+    } else if (panelType === 'sub-peer-review') {
       return $localize`:@@shared.activityCollapseForAriaLabel:Collapse for`
+    } else if (panelType === 'side-bar' && args) {
+      // emails-panel, websites-panel, personal-identifiers-panel, keywords-panel, countries-panel
+      if (args === 'emails-panel') {
+        return $localize`:@@shared.emailsCollapseAriaLabel:Collapse the Emails section`
+      } else if (args === 'websites-panel') {
+        return $localize`:@@shared.websiteCollapseAriaLabel:Collapse the Website & Social links section`
+      } else if (args === 'personal-identifiers-panel') {
+        return $localize`:@@shared.personalIdsCollapseAriaLabel:Collapse the Personal identifiers section`
+      } else if (args === 'keywords-panel') {
+        return $localize`:@@shared.keywordsCollapseAriaLabel:Collapse the Keywords section`
+      } else if (args === 'countries-panel') {
+        return $localize`:@@shared.countriesCollapseAriaLabel:Collapse the Countries section`
+      }
     } else {
-      return $localize`:@@shared.hideDetails:Hide details`
+      return $localize`:@@shared.hideDetails:Hide details` + panelType
     }
   }
 }
--- src/app/shared/pipes/app-panels-expand-aria-label/app-panels-expand-aria-label.pipe.ts
+++ src/app/shared/pipes/app-panels-expand-aria-label/app-panels-expand-aria-label.pipe.ts
@@ -4,21 +4,38 @@ import { Pipe, PipeTransform } from '@angular/core'
   name: 'appPanelsExpandAriaLabel',
 })
 export class AppPanelsExpandAriaLabelPipe implements PipeTransform {
-  transform(appType: any, args?: any): string {
-    if (appType == 'employment') {
+  transform(panelType: any, args?: any): string {
+    if (panelType === 'employment') {
       return $localize`:@@shared.employmentExpandAriaLabel:Expand the Employment section`
-    } else if (appType == 'education' || appType == 'qualification') {
+    } else if (panelType === 'education' || panelType === 'qualification') {
       return $localize`:@@shared.educationQualificationExpandAriaLabel:Expand the Education and qualifications section`
-    } else if (appType == 'distinction' || appType == 'invited-position') {
+    } else if (
+      panelType === 'distinction' ||
+      panelType === 'invited-position'
+    ) {
       return $localize`:@@shared.distinctionInvitedPositionExpandAriaLabel:Expand the Invited positions and distinction section`
-    } else if (appType == 'membership' || appType == 'service') {
+    } else if (panelType === 'membership' || panelType === 'service') {
       return $localize`:@@shared.membershipServiceExpandAriaLabel:Expand the Membership and service section`
-    } else if (appType == 'funding') {
+    } else if (panelType === 'funding') {
       return $localize`:@@shared.fundingServiceExpandAriaLabel:Expand the Funding section`
-    } else if (appType == 'works') {
+    } else if (panelType === 'works') {
       return $localize`:@@shared.workServiceExpandAriaLabel:Expand the Works section`
-    } else if (appType == 'peer-review' || appType == 'sub-peer-review') {
+    } else if (panelType === 'peer-review' || panelType === 'sub-peer-review') {
       return $localize`:@@shared.peerReviewServiceExpandAriaLabel:Expand the Peer review section`
+    } else if (panelType === 'side-bar' && args) {
+      console.log(panelType, args)
+
+      if (args === 'emails-panel') {
+        return $localize`:@@shared.emailsExpandAriaLabel:Expand the Emails section`
+      } else if (args === 'websites-panel') {
+        return $localize`:@@shared.websiteExpandAriaLabel:Expand the Website & Social links section`
+      } else if (args === 'personal-identifiers-panel') {
+        return $localize`:@@shared.personalIdsExpandAriaLabel:Expand the Personal identifiers section`
+      } else if (args === 'keywords-panel') {
+        return $localize`:@@shared.keywordsExpandAriaLabel:Expand the Keywords section`
+      } else if (args === 'countries-panel') {
+        return $localize`:@@shared.countriesExpandAriaLabel:Expand the Countries section`
+      }
     } else {
       return $localize`:@@shared.showDetails:Show details`
     }
