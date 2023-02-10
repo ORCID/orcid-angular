be964430dbac15f9519ae0de9651ccb30719ee66
Karen Madrigal <kprismm@gmail.com>
2023-02-10 11:41:11 -0600

    variables for sensitive data

--- cypress/e2e/api/orcid_internal.cy.js
+++ cypress/e2e/api/orcid_internal.cy.js
@@ -1,22 +1,28 @@
 
 /// <reference types="cypress" />
 import { qase } from 'cypress-qase-reporter/dist/mocha'
+import userData from '../../fixtures/testing-users.fixture.json'
 
 describe('Scope /orcid-internal', async function () {
 //test requires VPN connection
 
     const curlStatement =
-      "curl -i -L -k -H 'Accept: application/json' --data 'client_id=APP-VZK0IUK07U32NDTK&client_secret=d3be4dbd-7cc4-4b83-94f4-6bb6da442ccf&grant_type=client_credentials&redirect_uri=https://qa.orcid.org/&scope=/premium-notification /orcid-internal' http://reg-qa-appall-dfw-x1.int.orcid.org:13103/orcid-internal-api/oauth/token"
+      "curl -i -L -k -H 'Accept: application/json' --data 'client_id="+
+      userData.cyAPI_orcid_internal.clientId+
+      "&client_secret="+
+      userData.cyAPI_orcid_internal.secret
+      +"&grant_type=client_credentials&redirect_uri=https://qa.orcid.org/&scope=/premium-notification /orcid-internal' http://reg-qa-appall-dfw-x1.int.orcid.org:13103/orcid-internal-api/oauth/token"
+    
     qase(
     '150',
-        it('Verify scope /orcid-internal returns an access token', function () {
-            cy.exec(curlStatement).then((response) => {
-                //verify curl was executed successfully
-                expect(response.code).to.eq(0)
-                expect(response.stdout).to.contain('HTTP/1.1 200')
-                expect(response.stdout).to.contain('access_token')
-            })
+    it('Verify scope /orcid-internal returns an access token', function () {
+        cy.exec(curlStatement).then((response) => {
+            //verify curl was executed successfully
+            expect(response.code).to.eq(0)
+            expect(response.stdout).to.contain('HTTP/1.1 200')
+            expect(response.stdout).to.contain('access_token')
         })
+    })
     )//qase
 
 })
