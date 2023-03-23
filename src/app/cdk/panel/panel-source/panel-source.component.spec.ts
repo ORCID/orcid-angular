eb1ef69936eb97d06cf3badcc1d9251d67105340
Daniel Palafox <dp852@hotmail.com>
2023-03-22 19:07:35 -0500

    test: Add missing provider in basic test

--- src/app/cdk/panel/panel-source/panel-source.component.spec.ts
+++ src/app/cdk/panel/panel-source/panel-source.component.spec.ts
@@ -11,6 +11,9 @@ import { MatSnackBar } from '@angular/material/snack-bar'
 import { MatDialog, MatDialogModule } from '@angular/material/dialog'
 import { Overlay } from '@angular/cdk/overlay'
 import { VerificationEmailModalService } from '../../../core/verification-email-modal/verification-email-modal.service'
+import {
+  AppPanelActivityActionAriaLabelPipe
+} from '../../../shared/pipes/app-panel-activity-action-aria-label/app-panel-activity-action-aria-label.pipe'
 
 describe('PanelSourceComponent', () => {
   let component: PanelSourceComponent
@@ -21,6 +24,7 @@ describe('PanelSourceComponent', () => {
       imports: [HttpClientTestingModule, MatDialogModule, RouterTestingModule],
       declarations: [PanelSourceComponent],
       providers: [
+        AppPanelActivityActionAriaLabelPipe ,
         WINDOW_PROVIDERS,
         VerificationEmailModalService,
         PlatformInfoService,
