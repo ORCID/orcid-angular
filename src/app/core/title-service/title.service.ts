f4f8ac3c1561b2dbc69bfca1fbf8bcf502cc3205
Leonardo Mendoza <leomendoza123@gmail.com>
2023-06-20 13:35:51 -0600

    8560-page-titles-accessibility

---
+++ src/app/core/title-service/title.service.ts
@@ -0,0 +1,66 @@
+import { Injectable, OnInit } from '@angular/core'
+import { Title } from '@angular/platform-browser'
+import { NavigationEnd, NavigationStart, Router } from '@angular/router'
+import { log } from 'console'
+import { ReplaySubject, combineLatest } from 'rxjs'
+import { filter, map, withLatestFrom } from 'rxjs/operators'
+import {
+  ApplicationDynamicRoutesLabels,
+  ApplicationRoutes,
+  ApplicationRoutesLabels,
+  ORCID_REGEXP,
+} from 'src/app/constants'
+
+@Injectable()
+export class TitleService {
+  private _displayName: ReplaySubject<string> = new ReplaySubject<string>(1)
+
+  constructor(private _titleService: Title, private _router: Router) {}
+
+  init() {
+    this._displayName.next('')
+    combineLatest([
+      this._router.events.pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map((event: NavigationEnd) => event as NavigationEnd)
+      ),
+      this._displayName.asObservable(),
+    ]).subscribe(([event, dynamicTitle]) => {
+      // PUBLIC PAGE
+      if (
+        !event?.url.startsWith('/my-orcid') &&
+        ORCID_REGEXP.test(event?.url) &&
+        dynamicTitle
+      ) {
+        this.setTitle(
+          `${dynamicTitle} ${ApplicationDynamicRoutesLabels.orcidPageTitle}`
+        )
+      } else {
+        Object.keys(ApplicationRoutesLabels).forEach((route) => {
+          if (event.url.startsWith('/' + route)) {
+            // MY ORCID
+            if (
+              ApplicationRoutesLabels[route] === '{dynamic}' &&
+              dynamicTitle
+            ) {
+              this.setTitle(
+                `${dynamicTitle} ${ApplicationDynamicRoutesLabels.orcidMyPageTitle}`
+              )
+            } else {
+              // OTHER PAGES
+              this.setTitle(ApplicationRoutesLabels[route])
+            }
+          }
+        })
+      }
+    })
+  }
+
+  setTitle(title: string) {
+    if (title) this._titleService.setTitle(title)
+  }
+
+  setDisplayName(displayedNameWithId: string) {
+    this._displayName.next(displayedNameWithId)
+  }
+}
