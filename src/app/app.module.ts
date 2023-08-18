0ee11f29e0cd52ca67bfa3c6daac56c4f61ff75a
amontenegro <a.montenegro@orcid.org>
2023-08-18 13:37:59 -0600

    Update the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { LayoutModule } from './layout/layout.module'
 import { BidiModule } from '@angular/cdk/bidi'
 import { PseudoModule } from 'src/locale/i18n.pseudo.component'
 import { TitleService } from './core/title-service/title.service'
-import { HttpHeaderInterceptor } from './core/http-header-interceptor/http-header-interceptor'
+import { HttpContentTypeHeaderInterceptor } from './core/http-content-type-header-interceptor/http-content-type-header-interceptor'
 import { HTTP_INTERCEPTORS } from '@angular/common/http'
 
 @NgModule({
@@ -27,7 +27,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http'
     // Environmental dependent modules
     environment.SHOW_TEST_WARNING_BANNER ? EnvironmentBannerModule : [],
   ],
-  providers: [TitleService, {provide:HTTP_INTERCEPTORS, useClass:HttpHeaderInterceptor, multi:true}],
+  providers: [TitleService, {provide:HTTP_INTERCEPTORS, useClass:HttpContentTypeHeaderInterceptor, multi:true}],
   bootstrap: [AppComponent],
 })
 export class AppModule {
