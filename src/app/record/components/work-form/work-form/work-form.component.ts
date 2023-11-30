61aa9cf4c4e97f4d737d387e1e011e83b9fd483d
amontenegro <a.montenegro@orcid.org>
2023-11-30 16:36:12 -0600

    Remove unecesary import

--- src/app/record/components/work-form/work-form/work-form.component.ts
+++ src/app/record/components/work-form/work-form/work-form.component.ts
@@ -52,7 +52,7 @@ import { SnackbarService } from 'src/app/cdk/snackbar/snackbar.service'
 import { WorkIdentifiers } from 'src/app/shared/validators/work-identifiers/work-identifiers.validator'
 import { workCitationValidator } from 'src/app/shared/validators/citation/work-citation.validator'
 import { translatedTitleValidator } from 'src/app/shared/validators/translated-title/translated-title.validator'
-import { merge, Subject, of, EMPTY } from 'rxjs'
+import { merge, Subject, of } from 'rxjs'
 import { RecordService } from 'src/app/core/record/record.service'
 
 @Component({
