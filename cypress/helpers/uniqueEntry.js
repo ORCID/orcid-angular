71ed732d18cd629c69d9e0b57df3651daf0c3ac2
Leonardo Mendoza Fernadez <l.mendoza@Leonardos-MacBook-Pro.local>
2023-12-15 11:44:54 -0600

    feature/clean-angular

--- cypress/helpers/uniqueEntry.js
+++
@@ -1,6 +0,0 @@
-// Generate a unique substring/identifier to append to a given entry
-module.exports = function () {
-  const uuid = () => Cypress._.random(0, 1e6)
-  const entryId = uuid()
-  return entryId
-}
