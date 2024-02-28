8b89c688e69550b0a5de1a1c97c37fd48411ee5e
Daniel Palafox <daniel.p4l4fox@gmail.com>
2024-02-28 13:00:55 -0600

    feature: Add new sign in updates behind togglz

--- src/app/cdk/my-orcid-alerts/my-orcid-alerts.module.ts
+++ src/app/cdk/my-orcid-alerts/my-orcid-alerts.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MyOrcidAlertsComponent } from './my-orcid-alerts.component'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [MyOrcidAlertsComponent],
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, MatIconModule],
   exports: [MyOrcidAlertsComponent],
 })
 export class MyOrcidAlertsModule {}
