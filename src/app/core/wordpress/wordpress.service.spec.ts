07be55989c6ab718dc2b48bf54ab5b8313a3be66
Leonardo Mendoza Fernadez <l.mendoza@Leonardos-MacBook-Pro.local>
2024-02-27 20:00:16 -0600

    WORDPRESS_HOME_PAGE

--- src/app/core/wordpress/wordpress.service.spec.ts
+++ src/app/core/wordpress/wordpress.service.spec.ts
@@ -1,16 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { WordpressService } from './wordpress.service';
+import { WordpressService } from './wordpress.service'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { PlatformInfoService } from 'src/app/cdk/platform-info'
+import { WINDOW_PROVIDERS } from 'src/app/cdk/window'
 
 describe('WordpressService', () => {
-  let service: WordpressService;
+  let service: WordpressService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(WordpressService);
-  });
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [WINDOW_PROVIDERS, PlatformInfoService],
+    })
+    service = TestBed.inject(WordpressService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+})
