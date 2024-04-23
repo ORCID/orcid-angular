d3683722e3322b81901b9ed2cc311c72ed41f4c4
Daniel Palafox <daniel.p4l4fox@gmail.com>
2024-04-23 12:51:25 -0600

    style: Fix spacing between page header footer and main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,7 @@ export class AppComponent {
   currentlyDisplayingZendesk = true
   headlessMode = false
   footerlessMode = false
+  spacing: boolean
   currentRouteIsHeadlessOnOauthPage = true
   screenDirection
   currentRoute
@@ -84,6 +85,7 @@ export class AppComponent {
       if (event instanceof NavigationStart) {
         reportNavigationStart(event.url)
         this.currentRoute = event.url
+        console.log('currentRoute', this.currentRoute)
       }
       if (event instanceof NavigationEnd) {
         const duration = finishPerformanceMeasurement(event.url)
@@ -108,6 +110,7 @@ export class AppComponent {
                 })
             }
           })
+        this.addSpacing()
       }
     })
   }
@@ -142,5 +145,19 @@ export class AppComponent {
   @HostListener('window:visibilitychange')
   onVisibilityChange(value) {
     this._userService.setTimerAsHiddenState(this._window.document.hidden)
+  }
+
+  private addSpacing() {
+    switch (this.currentRoute) {
+      case '/signin':
+      case '/register':
+      case '/authorize':
+      case '/search':
+        this.spacing = true
+      break
+      default:
+        this.spacing = false
+      break
+    }
   }
 }
