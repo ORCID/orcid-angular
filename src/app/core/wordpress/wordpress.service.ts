ee3bf6d6ff0259234af3ecd8a7493b4130bf8805
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2024-05-27 17:53:13 -0600

    enable-homepage-prod

--- src/app/core/wordpress/wordpress.service.ts
+++ src/app/core/wordpress/wordpress.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http'
 import { Inject, Injectable, LOCALE_ID } from '@angular/core'
+import { catchError, map } from 'rxjs/operators'
 import { WordpressPost } from 'src/app/types/wordpress'
 import { environment } from 'src/environments/environment'
 
@@ -13,12 +14,46 @@ export class WordpressService {
   ) {}
 
   getHomePagePost() {
-    return this.httpClient.get(
-      `${
-        environment.WORDPRESS_S3
-      }/index${this.getWordpressLocalizationCode()}.html`,
-      { responseType: 'text' }
-    )
+    const primaryUrl = `${
+      environment.WORDPRESS_S3
+    }/index${this.getWordpressLocalizationCode()}.html`
+    const fallbackUrl = `${
+      environment.WORDPRESS_S3_FALLBACK
+    }/index${this.getWordpressLocalizationCode()}.html`
+
+    return this.httpClient
+      .get(primaryUrl, { responseType: 'text' })
+      .pipe(
+        map((html: string) => {
+          return {
+            html: html,
+            url: environment.WORDPRESS_S3,
+          }
+        })
+      )
+      .pipe(
+        catchError((error) => {
+          console.error('Primary URL failed, trying fallback URL:', error)
+          return this.httpClient
+            .get(fallbackUrl, { responseType: 'text' })
+            .pipe(
+              map((html: string) => {
+                return {
+                  html: html,
+                  url: environment.WORDPRESS_S3_FALLBACK,
+                }
+              })
+            )
+        }),
+        // Update resulting HTML to use the correct base URL
+        map((data: { html: string; url: string }) => {
+          const find = './assets/'
+          const regex = new RegExp(find, 'g')
+          console.log('html', data.html)
+          const updatedHtml = data.html.replace(regex, data.url + '/assets/')
+          return updatedHtml
+        })
+      )
   }
   getWordpressLocalizationCode() {
     if (this.locale.includes('en')) {
