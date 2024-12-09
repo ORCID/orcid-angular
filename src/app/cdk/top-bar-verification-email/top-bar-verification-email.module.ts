4c724800f7631fc44bddbdb38b23092f48542bc4
andrej romanov <50377758+auumgn@users.noreply.github.com>
2024-12-09 11:55:51 +0200

    replace my orcid verification panels

--- src/app/cdk/top-bar-verification-email/top-bar-verification-email.module.ts
+++ src/app/cdk/top-bar-verification-email/top-bar-verification-email.module.ts
@@ -5,6 +5,7 @@ import { ModalModule } from '../modal/modal.module'
 import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button'
 import { MatIconModule } from '@angular/material/icon'
 import { TopBarVerificationEmailModalComponent } from './modals/top-bar-verification-email-modal/top-bar-verification-email-modal.component'
+import { AlertMessageModule } from '../alert-message/alert-message.module'
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { TopBarVerificationEmailModalComponent } from './modals/top-bar-verifica
   ],
   imports: [
     CommonModule,
+    AlertMessageModule,
     ModalModule,
     MatButtonModule,
     MatIconModule,
