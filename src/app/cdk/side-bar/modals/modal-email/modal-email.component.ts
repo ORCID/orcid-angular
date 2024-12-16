a8f71daa5f38dd1a9ce9f015f186ef012551c7e6
andrej romanov <50377758+auumgn@users.noreply.github.com>
2024-12-16 17:12:42 +0200

    add subdomain support to the email modal

--- src/app/cdk/side-bar/modals/modal-email/modal-email.component.ts
+++ src/app/cdk/side-bar/modals/modal-email/modal-email.component.ts
@@ -486,7 +486,9 @@ export class ModalEmailComponent implements OnInit, OnDestroy {
 
       if (domain) {
         const remainingEmailsHaveDomain = this.emails.find((email) =>
-          email ? email.verified && email.value.split('@')[1] === domain : false
+          email
+            ? email.verified && email.value.split('@')[1].endsWith(domain)
+            : false
         )
         if (!remainingEmailsHaveDomain) {
           this.verifiedDomains = this.verifiedDomains.filter(
@@ -579,7 +581,7 @@ export class ModalEmailComponent implements OnInit, OnDestroy {
         if (controlKey.startsWith('emailInput')) {
           const control = this.emailsForm.get(controlKey).value
 
-          if (control.email.split('@')[1] === domain) {
+          if (control.email.split('@')[1].endsWith(domain)) {
             visibilities.push(control.visibility)
           }
         }
@@ -612,7 +614,7 @@ export class ModalEmailComponent implements OnInit, OnDestroy {
       Object.keys(this.emailsForm.controls).forEach((controlKey) => {
         if (controlKey.startsWith('emailInput')) {
           const control = this.emailsForm.get(controlKey)
-          if (control.value.email.split('@')[1] === domain) {
+          if (control.value.email.split('@')[1].endsWith(domain)) {
             if (this.isEmailVerified(control.value.email)) {
               visibilities.push(control.value.visibility)
             }
