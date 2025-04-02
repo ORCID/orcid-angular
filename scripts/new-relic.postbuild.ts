a47813870ddd474bdeb036165e6dd807a886dac5
Leonardo Mendoza <leomendoza123@gmail.com>
2025-04-01 21:52:35 -0600

    enable-new-relic

---
+++ scripts/new-relic.postbuild.ts
@@ -0,0 +1,12 @@
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+const newRelicScript = readFileSync(
+  join(__dirname, 'new-relic.runtime.js'),
+  'utf8'
+)
+
+export function newRelic(indexHtml: string) {
+  return indexHtml.replace('<!-- NEW_RELIC_PLACEHOLDER -->', `<script type="text/javascript">
+` + newRelicScript + `</script>`)
+}
