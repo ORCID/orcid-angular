9037d6617dd27b767af5bb59f2d2250a52178546
Camelia Dumitru <c.dumitru@orcid.org>
2025-04-09 09:08:31 +0100

    Fixed the navigation after sign in

--- src/app/sign-in/components/form-sign-in/form-sign-in.component.ts
+++ src/app/sign-in/components/form-sign-in/form-sign-in.component.ts
@@ -199,7 +199,7 @@ export class FormSignInComponent implements OnInit, OnDestroy {
           if (isRedirectToTheAuthorizationPage(data)) {
             this.handleOauthLogin(data.url)
           } else {
-                () => this.navigateTo(data.url)
+              this.navigateTo(data.url)
           }
         } else if (data.verificationCodeRequired && !data.badVerificationCode) {
           this.authorizationFormSubmitted = false
