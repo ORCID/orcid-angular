47e07dd412a0cc4605705110e26e31b8bab81b35
andrej romanov <50377758+auumgn@users.noreply.github.com>
2025-04-21 20:01:34 +0300

    restore third party signin routing logic (#2501)
    * restore third party signin routing logic
    
    * restore oauth redirect logic

---
+++ src/app/guards/third-party-signin-completed.guard.spec.ts
@@ -0,0 +1,38 @@
+import { TestBed } from '@angular/core/testing'
+
+import { ThirdPartySigninCompletedGuard } from './third-party-signin-completed.guard'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog'
+import { WINDOW_PROVIDERS } from '../cdk/window'
+import { PlatformInfoService } from '../cdk/platform-info'
+import { ErrorHandlerService } from '../core/error-handler/error-handler.service'
+import { SnackbarService } from '../cdk/snackbar/snackbar.service'
+import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar'
+import { Overlay } from '@angular/cdk/overlay'
+
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+
+describe('ThirdPartySigninCompletedGuard', () => {
+  let guard: ThirdPartySigninCompletedGuard
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        WINDOW_PROVIDERS,
+        PlatformInfoService,
+        ErrorHandlerService,
+        SnackbarService,
+        MatSnackBar,
+        MatDialog,
+        Overlay,
+      ],
+    })
+    guard = TestBed.inject(ThirdPartySigninCompletedGuard)
+  })
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy()
+  })
+})
