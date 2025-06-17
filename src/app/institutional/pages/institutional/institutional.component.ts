097c089908ae0060a971b1ba18a1783f4a05887c
andrej romanov <50377758+auumgn@users.noreply.github.com>
2025-06-17 19:21:23 +0300

    Revert "Reapply "material mdc migration batch 1""
    This reverts commit edbade8626f5881353d664fd6fc82fb8b03955aa.

--- src/app/institutional/pages/institutional/institutional.component.ts
+++ src/app/institutional/pages/institutional/institutional.component.ts
@@ -10,7 +10,7 @@ import {
   UntypedFormGroup,
   Validators,
 } from '@angular/forms'
-import { MatAutocompleteTrigger as MatAutocompleteTrigger } from '@angular/material/autocomplete'
+import { MatLegacyAutocompleteTrigger as MatAutocompleteTrigger } from '@angular/material/legacy-autocomplete'
 import { Router } from '@angular/router'
 import { CookieService } from 'ngx-cookie-service'
 import { Observable, Subject } from 'rxjs'
