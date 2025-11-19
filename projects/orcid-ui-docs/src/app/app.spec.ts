e9db8c3024886b1871e5a4aeb9c07ed212295aa7
Leonardo Mendoza <leomendoza123@gmail.com>
2025-11-19 00:05:50 -0600

    PD-2235

--- projects/orcid-ui-docs/src/app/app.spec.ts
+++ projects/orcid-ui-docs/src/app/app.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
 import { App } from './app'
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App, RouterTestingModule],
     }).compileComponents()
   })
 
@@ -19,7 +20,7 @@ describe('App', () => {
     fixture.detectChanges()
     const compiled = fixture.nativeElement as HTMLElement
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, orcid-ui-docs'
+      'Orcid UI'
     )
   })
 })
