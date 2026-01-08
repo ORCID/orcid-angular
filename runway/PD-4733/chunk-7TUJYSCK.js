862c7d43983314e821abef4152ce3353fb4c3c51
cryptalith <cryptalith@users.noreply.github.com>
2026-01-08 19:59:49 +0000

    deploy: 5bb1a1d4ec7e17711648b00a0458ba58dc63b874

---
+++ runway/PD-4733/chunk-7TUJYSCK.js
@@ -0,0 +1,1 @@
+import{fb as e}from"./chunk-7LL6HRWC.js";var n=(()=>{class t{static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275dir=e({type:t,selectors:[["","accentButton",""]],hostAttrs:[1,"accent-button"]})}}return t})();export{n as a};
